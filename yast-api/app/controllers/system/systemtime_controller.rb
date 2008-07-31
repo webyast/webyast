76aba436a92701f16d4d50b71e16b0a8a7d7191d
Stefan Schubert <schubi@waerden.suse.de>
2008-07-31 03:38:11 +0200

    using SCR calls

--- yast-api/app/controllers/system/systemtime_controller.rb
+++ yast-api/app/controllers/system/systemtime_controller.rb
@@ -1,3 +1,5 @@
+require "dbus"
+
 class System::SystemtimeController < ApplicationController
 	
   def update
@@ -5,7 +7,57 @@ class System::SystemtimeController < ApplicationController
       systemtime = System::SystemTime.new
       if systemtime.update_attributes(params[:system_time])
         logger.debug "UPDATED: #{systemtime.inspect}"
-        #system("date -s ", params[:systemtime][:time])
+        system_bus = DBus::SystemBus.instance
+
+        # Get the yast service
+        yast = system_bus.service("org.opensuse.yast.SCR")
+
+        # Get the object from this service
+        objYast = yast.object("/SCR")
+        poiSCR = DBus::ProxyObjectInterface.new(objYast, 
+                                                "org.opensuse.yast.SCR.Methods")
+        poiSCR.define_method("Write", 
+                             "in path:(bsv), in arg:(bsv), in opt:(bsv), out ret:(bsv)")
+        poiSCR.define_method("Execute", 
+                             "in path:(bsv), in arg:(bsv), in opt:(bsv), out ret:(bsv)")
+
+        #set hwclock
+	if systemtime.is_utc
+	  hwclock = "-u" 
+        else
+          hwclock = "--localtime"
+        end
+        poiSCR.Write([false, "path", ["s",".sysconfig.clock.HWCLOCK"] ], 
+                     [false, "string", ["s",hwclock] ], 
+                     [false, "", ["s",""] ])
+
+        #set timezone
+        poiSCR.Write([false, "path", ["s",".sysconfig.clock.TIMEZONE"] ], 
+                     [false, "string", ["s",systemtime.timezone] ], 
+                     [false, "", ["s",""] ])
+
+        #set time
+	cmd = "";
+	if (systemtime.timezone.length >0 && hwclock != "--localtime")
+	    cmd = "TZ=" + systemtime.timezone + " "
+        end
+
+	cmd = cmd + "/sbin/hwclock --set " + hwclock + 
+              " --date=\"#{systemtime.systemtime.month}/#{systemtime.systemtime.day}/#{systemtime.systemtime.year}" +
+              " #{systemtime.systemtime.hour}:#{systemtime.systemtime.min}:#{systemtime.systemtime.sec}\""
+
+	logger.debug "SetTime cmd #{cmd}"
+        poiSCR.Execute([false, "path", ["s",".target.bash_output"] ], 
+                       [false, "string", ["s",cmd] ], 
+                       [false, "", ["s",""] ])
+
+	cmd = "/sbin/hwclock --hctosys " + hwclock;
+
+	logger.debug "SetTime cmd #{cmd}"
+        poiSCR.Execute([false, "path", ["s",".target.bash_output"] ], 
+                       [false, "string", ["s",cmd] ], 
+                       [false, "", ["s",""] ])
+
         format.html { redirect_to :action => "show" }
 	format.json { head :ok }
 	format.xml { head :ok }
@@ -17,22 +69,48 @@ class System::SystemtimeController < ApplicationController
     end
   end
 
-  # Workaround for put-problem
-  def create
-    logger.error("TTTTEEEEEESSSSSSST", params[:systemtime][:time])	
-  end
+# Workaround for put-problem
+#  def create
+#    logger.error("create ....", params[:systemtime][:time])	
+#  end
 
   def show
+    system_bus = DBus::SystemBus.instance
+
+    # Get the yast service
+    yast = system_bus.service("org.opensuse.yast.SCR")
+
+    # Get the object from this service
+    objYast = yast.object("/SCR")
+    poiSCR = DBus::ProxyObjectInterface.new(objYast, 
+                                            "org.opensuse.yast.SCR.Methods")
+    poiSCR.define_method("Read", 
+                         "in path:(bsv), in arg:(bsv), in opt:(bsv), out ret:(bsv)")
+    poiSCR.define_method("Execute", 
+                         "in path:(bsv), in arg:(bsv), in opt:(bsv), out ret:(bsv)")
+
     @systemtime = System::SystemTime.new
-    @systemtime.id = "systemtime"
-    @systemtime.systemtime = `date +%r`
-    temp = `date +%Z`
-    if temp == "UTC" then
+
+    ret = poiSCR.Execute([false, "path", ["s",".target.bash_output"] ], 
+              [false, "string", ["s","/bin/date"] ], 
+              [false, "", ["s",""] ])
+
+    @systemtime.systemtime = ret[0][2]["stdout"][2]
+
+    ret = poiSCR.Read([false, "path", ["s",".sysconfig.clock.HWCLOCK"] ],
+              [false, "", ["s",""] ], 
+              [false, "", ["s",""] ])
+
+    if ret[0][2] == "-u" then
       @systemtime.is_utc = true
     else
       @systemtime.is_utc = false
     end
-    @systemtime.timezone = `date +%Z`.chomp
+
+    ret = poiSCR.Read([false, "path", ["s",".sysconfig.clock.TIMEZONE"] ],
+              [false, "", ["s",""] ], 
+              [false, "", ["s",""] ])
+    @systemtime.timezone = ret[0][2]
 
     respond_to do |format|
       format.xml do
