800e87c226f1ca690da39953f347ac02b9c9845f
Stefan Schubert <schubi@waerden.suse.de>
2008-10-07 11:59:58 +0200

    script for setting access rules for a user

---
+++ dist/policyKit-rights.rb
@@ -0,0 +1,101 @@
+#!/usr/bin/ruby
+#
+# policyKit-rights.rb
+#
+# show, grant and revoke policies for YaST webservice
+#
+# run: ruby policyKit-rights.rb
+#
+#
+require 'fileutils'
+require 'getoptlong'
+
+$debug = 0
+
+def usage why
+	STDERR.puts why
+	STDERR.puts "Usage: policyKit-right.rb --user <user> --action (show|grant|revoke)"
+        STDERR.puts "NOTE: This programm should be run by user root"
+        STDERR.puts ""
+        STDERR.puts "This call grant/revoke ALL permission for YaST Webservice."
+        STDERR.puts "In order to grant/revoke single rights use:"
+        STDERR.puts "polkit-auth --user <user> (--grant|-revoke) <policyname>"
+        STDERR.puts ""
+        STDERR.puts "In order to show all possible permissions use:"
+        STDERR.puts "polkit-action"
+	exit 1
+end
+
+options = GetoptLong.new(
+  [ "--user",   GetoptLong::REQUIRED_ARGUMENT ],
+  [ "--action", GetoptLong::REQUIRED_ARGUMENT ]
+)
+
+user = nil
+action = nil
+
+
+begin
+options.each do |opt, arg|
+  case opt
+    when "--user": user = arg
+    when "--action": action = arg
+    when "--debug": $debug += 1
+    else
+	STDERR.puts "Ignoring unrecognized option #{opt}"
+  end
+end
+rescue
+end
+
+$debug = nil if $debug == 0
+
+usage "excessive arguments" unless ARGV.empty?
+usage "--user parameter missing" unless user
+usage "--action parameter (show|grant|revoke) missing" unless action
+
+begin
+   if action == "grant"
+      IO.popen( "polkit-action", 'r+' ) do |pipe|
+         loop do
+            suseString = "org.opensuse.yast.webservice."
+            break if pipe.eof?
+            l = pipe.read
+            policies = l.split("\n")
+            policies.each do |policy|
+               if policy.include? suseString
+                  policySplit = policy.split("-")
+                  if policySplit.size >= 2
+                     command = "polkit-auth --user " + user + " --explicit |grep -s " + policySplit[0] + "-" + policySplit[1] + " >>/dev/null"
+                     if !system(command) # has not already been set
+                       STDERR.puts "granting: #{policy}"
+                       command = "polkit-auth --user " + user + " --grant " + policySplit[0] + "-" + policySplit[1]
+                       system (command)
+                     end
+                  end
+               end
+            end
+         end
+      end
+   else
+      command = "polkit-auth --user " + user + " --explicit"
+      IO.popen( command, 'r+' ) do |pipe|
+         loop do
+            break if pipe.eof?
+            l = pipe.read
+            case action
+               when "show"
+                  STDERR.puts l
+               when "revoke"
+                  policies = l.split("\n")
+                  policies.each do |policy|
+                    STDERR.puts "revoking: #{policy}"
+                    command = "polkit-auth --user " + user + " --revoke " + policy
+                    system (command)
+                 end
+            end
+         end
+      end
+   end
+end
+
---
+++ webservice/test/functional/permission_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class PermissionControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
