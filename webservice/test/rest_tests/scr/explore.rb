46fe42b55ead62342a6135e59af7fc959d36873f
Klaus Kämpf <kkaempf@suse.de>
2009-04-17 12:36:07 +0200

    Better D-Bus exploration

--- webservice/test/rest_tests/scr/explore.rb
+++ webservice/test/rest_tests/scr/explore.rb
@@ -29,14 +29,20 @@ def type2s t
     "bool"
   when "i"
     "int"
+  when "u"
+    "unsigned int"
   when "o"
     "objectpath"
   when "s"
     "string"
   when "v"
     "variant"
+  when "y"
+    "byte"
   when "a"
     type2s(ts.shift) + "[]"
+  when nil
+    "void"
   else
     "?#{x}"
   end
@@ -44,7 +50,7 @@ end
 
 
 def parm2s params
-  return "" if params.empty?
+  return "void" if params.empty?
   p = params.shift
   r = "#{type2s p[1]} #{p[0]}"
   unless params.empty?
@@ -54,34 +60,47 @@ def parm2s params
 end
 
 def meth2s meth
-  "#{parm2s meth.rets} #{meth.name}(#{parm2s meth.params})"
+  "\t#{parm2s meth.rets} #{meth.name}(#{parm2s meth.params})"
 end
 
-def explore_subnodes obj, prefix = ""
-  obj.subnodes.each do |p|
-    path = prefix + "/" + p
-    puts "Object #{path}"
-    node = obj.bus.introspect( obj.destination, path )
-    node.interfaces.each do |iface|
-      unless $known_interfaces.include? iface
-	puts "  #{iface}" 
-	node[iface].methods.each do |k,v|
-	  puts meth2s(v)
-	end
-	$known_interfaces << iface
+
+def explore_object obj
+  puts "Object #{obj.path}"
+  obj.interfaces.each do |iface|
+    puts "  #{iface}" 
+    unless $known_interfaces.include? iface
+      obj[iface].methods.each do |k,v|
+	puts meth2s(v)
       end
+      $known_interfaces << iface
     end
-    explore_subnodes node, path
+  end
+  if obj.subnodes
+    obj.subnodes.each do |p|
+      path = (obj.path.size == 1) ? "#{obj.path}#{p}" : "#{obj.path}/#{p}"
+      explore_object obj.bus.introspect(obj.destination, "#{path}")
+    end
   end
 end
 
-# Get the system bus
-bus = DBus::SystemBus.instance
+service = ARGV.shift
 
-if ARGV.empty?
-  $stderr.puts "Usage: ruby explore <service>"
+if service == "--session"
+  bus = DBus::session_bus
+  service = ARGV.shift
+else
+# Get the system bus
+  bus = DBus::system_bus
+end
+
+puts "Bus #{bus}"
+
+unless service
+  $stderr.puts "Usage: ruby explore [--session] <service>"
+  $stderr.puts " to explore the system (resp. session) bus"
   $stderr.puts "  where <service> is one of"
   i = 0
+  explore_object bus.introspect "org.freedesktop.DBus", "/"
   bus.proxy.ListNames[0].each do |service|
     $stderr.print "#{service}"
     if i % 3 == 0
@@ -93,14 +112,7 @@ if ARGV.empty?
   exit
 end
 
-service = ARGV.shift
 
-# Connect to the SCR service at the root object path
-proxy = bus.introspect service, "/"
 
-puts "Proxy <#{proxy}>"
-puts "path  <#{proxy.path}>"
-puts "destination  <#{proxy.destination}>"
-puts "default_iface  <#{proxy.default_iface}>" if proxy.default_iface
 
-explore_subnodes proxy
+explore_object bus.introspect( service, "/" )
