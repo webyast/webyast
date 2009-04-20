9200e398c6395494e3423634a54ce62022129207
Stefan Schubert <schubi@suse.de>
2009-04-20 18:41:36 +0200

    framwork for services plugin

--- webservice/config/routes.rb
+++ webservice/config/routes.rb
@@ -39,19 +39,6 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
 
-  map.resource :config, :controller => 'config_ntp', :path_prefix => "/services/ntp"
-  map.connect "/services/ntp/config/:id", :controller => 'config_ntp', :action => 'singlevalue'
-  map.connect "/services/ntp/config/:id.xml", :controller => 'config_ntp', :action => 'singlevalue', :format =>'xml'
-  map.connect "/services/ntp/config/:id.html", :controller => 'config_ntp', :action => 'singlevalue', :format =>'html'
-  map.connect "/services/ntp/config/:id.json", :controller => 'config_ntp', :action => 'singlevalue', :format =>'json'
-
-  map.namespace :services do |service|
-      service.resource :dummy
-  end
-
-  map.resources :services do |service|
-     service.resources :commands
-  end
 
   map.resources :users do |users|
     users.resources :permissions
