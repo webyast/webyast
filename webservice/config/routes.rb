a61999dc701e2831651f079df65d2ec86d638446
Stefan Schubert <schubi@suse.de>
2009-04-21 14:45:14 +0200

    added framwork for users plugin

--- webservice/config/routes.rb
+++ webservice/config/routes.rb
@@ -39,26 +39,6 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
 
-
-  map.resources :users do |users|
-    users.resources :permissions
-  end
-
-  map.resources :users, :member => { :exportssh => :get }
-  map.resources :users, :controller => 'users'
-
-  #required by ActiveResource
-  map.connect "/users/:users_id/update.xml", :controller => 'users', :action => 'update', :format =>'xml'
-  map.connect "/users/:users_id/update.json", :controller => 'users', :action => 'update', :format =>'json'
-  map.connect "/users/new/create.xml", :controller => 'users', :action => 'create', :format =>'xml'
-  map.connect "/users/new/create.json", :controller => 'users', :action => 'create', :format =>'json'
-
-  map.connect "/users/:users_id/exportssh", :controller => 'users', :action => 'exportssh'
-  map.connect "/users/:users_id/:id", :controller => 'users', :action => 'singlevalue'
-  map.connect "/users/:users_id/:id.xml", :controller => 'users', :action => 'singlevalue', :format =>'xml'
-  map.connect "/users/:users_id/:id.html", :controller => 'users', :action => 'singlevalue', :format =>'html'
-  map.connect "/users/:users_id/:id.json", :controller => 'users', :action => 'singlevalue', :format =>'json'
-
   map.resources :patch_updates
   map.connect "/patch_updates/:id", :controller => 'patch_updates', :action => 'install'
 
