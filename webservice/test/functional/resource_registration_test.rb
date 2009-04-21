131f4976afef25980768ce61f8a1aaeca08dd633
Klaus Kämpf <kkaempf@suse.de>
2009-04-21 16:40:25 +0200

    Test dynamic resource creation from yaml files
    Resources are described by yaml (either .yaml or .yml extension) files
    which contain name, domain (aka namespace), and tags. All these values
    can be used by clients to find the resources they need.
    
    Specifying resource domain or name explicitly is optional. The yaml
    file name give a default for the name, the parent directory the
    default for the domain.

--- webservice/test/functional/resource_registration_test.rb
+++ webservice/test/functional/resource_registration_test.rb
@@ -9,11 +9,64 @@ class ResourceRegistrationTest < ActiveSupport::TestCase
   
   fixtures :domains, :resources
   
-  # Create resource from .yaml file
+  # ResourceRegistration.init drops all database content
   
-  test "resource creation" do
+  test "resource initialization" do
     ResourceRegistration.init
     assert Domain.find(:all).empty?
     assert Resource.find(:all).empty?
+  end
+  
+  # Create resources from .yaml file
+  
+  test "resource creation" do
+    ResourceRegistration.init
+    ResourceRegistration.register_all ".", "resource_fixtures/good"
+      
+    assert !Domain.find(:all).empty?
+    assert !Resource.find(:all).empty?
+  end
+  
+  # Check if file name sets resource name
+  
+  test "resource name creation" do
+    ResourceRegistration.init
+    ResourceRegistration.register_all ".", "resource_fixtures/check_name"
+      
+    domains = Domain.find(:all)
+    assert domains
+    assert domains.size == 1
+    assert domains[0].name == "domain"
+    
+    resources = Resource.find(:all)
+    assert resources
+    assert resources.size == 1
+    assert resources[0].name == "resource"
+  end
+  
+  # Check if parent dir name sets domain name
+  
+  test "resource domain creation" do
+    ResourceRegistration.init
+    ResourceRegistration.register_all ".", "resource_fixtures/check_domain"
+      
+    domains = Domain.find(:all)
+    assert domains
+    assert domains.size == 1
+    assert domains[0].name == "domain"
+    
+    resources = Resource.find(:all)
+    assert resources
+    assert resources.size == 1
+    assert resources[0].name == "resource"
+  end
+  
+  # Catch errors in bad_domain
+  
+  test "resource exception" do
+    ResourceRegistration.init
+    assert_raise RuntimeError do
+      ResourceRegistration.register_all ".", "test/resource_fixtures/bad_domain"
+    end
   end
 end
