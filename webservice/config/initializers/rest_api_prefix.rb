b04349a62ac20070cecc8a5f7f125bb18e7d8970
Klaus Kämpf <kkaempf@suse.de>
2009-04-22 12:36:43 +0200

    remove version from URI prefix

--- webservice/config/initializers/rest_api_prefix.rb
+++ webservice/config/initializers/rest_api_prefix.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 #
-# Set the version of the REST interface.
+# Set the prefix for RESTful access
 #
-ActionController::Base.relative_url_root = "/yast/v1"
+ActionController::Base.relative_url_root = "/yast"
