83636151123409dedf90fad2f998a1e2b24b4973
Klaus Kämpf <kkaempf@suse.de>
2009-04-22 17:19:22 +0200

    Decrease log verbosity

--- webservice/config/routes.rb
+++ webservice/config/routes.rb
@@ -7,4 +7,4 @@ require "lib/resource_registration"
 # Don't route outside full Rails (e.g. when just running db:migrate)
 ResourceRegistration.route_all if ENV["RAILS_ENV"]
 
-$stderr.puts ActionController::Routing::Routes.routes if ENV["RAILS_ENV"] == "development"
+#$stderr.puts ActionController::Routing::Routes.routes if ENV["RAILS_ENV"] == "development"
