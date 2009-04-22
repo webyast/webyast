28f1264f557a515cabc43c3ba0ff3db918d47d96
Klaus Kämpf <kkaempf@suse.de>
2009-04-22 13:10:52 +0200

    Disable route setup outside rails, e.g in db:migrate

--- webservice/config/routes.rb
+++ webservice/config/routes.rb
@@ -2,7 +2,9 @@ require "lib/resource"
   
 # routing is generated from the 'resources' database table
 # see config/initializers/resource_registration.rb for populating the database
-  
-ResourceRegistration.route_all
+
+
+# Don't route outside full Rails (e.g. when just running db:migrate)
+ResourceRegistration.route_all if ENV["RAILS_ENV"]
 
 $stderr.puts ActionController::Routing::Routes.routes if ENV["RAILS_ENV"] == "development"
