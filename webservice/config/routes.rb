7ce6777426619689119235ff22c2f129919287f8
Duncan Mac-Vicar P <dmacvicar@suse.de>
2009-04-24 00:29:49 +0200

    fix login

--- webservice/config/routes.rb
+++ webservice/config/routes.rb
@@ -7,3 +7,7 @@ require "lib/resource_registration"
 # Don't route outside full Rails (e.g. when just running db:migrate)
 ResourceRegistration.route_all
 
+# route sessions statically, it is a singleton controller
+ActionController::Routing::Routes.draw do |map|
+  map.resource :session
+end
