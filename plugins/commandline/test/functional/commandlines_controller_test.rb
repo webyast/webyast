4153cc2e187e6e94f207308b6472510fe3ab6b82
Klaus Kämpf <kkaempf@suse.de>
2009-05-07 21:25:14 +0200

    Get plugin tests working
    This enables plugin tests with a faked session login.
    The fixtures must be at RAILS_PARENT as they're loaded relative to
    config/environment. But thats another issue to fix.

--- plugins/commandline/test/functional/commandlines_controller_test.rb
+++ plugins/commandline/test/functional/commandlines_controller_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class CommandlinesControllerTest < ActionController::TestCase
+  fixtures :accounts
   def setup
     @controller = Yast::CommandlinesController.new
+    @request = ActionController::TestRequest.new
+    # http://railsforum.com/viewtopic.php?id=1719
+    @request.session[:account_id] = 1 # defined in fixtures
   end
   
   # Replace this with your real tests.
