be2c4280dca98c8eb65ea75c38f3484b1555de3e
Klaus Kämpf <kkaempf@suse.de>
2009-05-07 15:21:18 +0200

    Fix test file name to match controller
    and add first test

---
+++ plugins/commandline/test/functional/commandlines_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CommandlinesControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  test "access index" do
+    get :index
+    assert_response :success
+  end
+end
