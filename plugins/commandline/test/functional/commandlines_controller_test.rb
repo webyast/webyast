468051b159cee52e2f656a6d88f9ad79ea68016b
Klaus Kämpf <kkaempf@suse.de>
2009-05-07 15:43:21 +0200

    Help finding the right controller

--- plugins/commandline/test/functional/commandlines_controller_test.rb
+++ plugins/commandline/test/functional/commandlines_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class CommandlinesControllerTest < ActionController::TestCase
+  def setup
+    @controller = Yast::CommandlinesController.new
+  end
+  
   # Replace this with your real tests.
   test "access index" do
     get :index
