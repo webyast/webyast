1aac2f90b07aaa6092e8bfc40c6b2a60f0631372
Klaus Kämpf <kkaempf@suse.de>
2009-05-08 12:20:17 +0200

    Use a partial, only a partial accepts :locals

--- webservice/lib/error_result.rb
+++ webservice/lib/error_result.rb
@@ -4,8 +4,10 @@
 
 class ErrorResult
   
+  # return parameters for 'render'
   def self.error ( status = 400, code = 1, message = "Unspecific")
-    { :layout => "error", :status => status, :code => code, :message => message }
+    $stderr.puts "Error #{code}:#{message}"
+    { :partial => "layouts/error", :status => status, :locals => { :code => code, :message => message } }
   end
   
 end
