9bced2b534c21714237aa73f38c6c14a92aeda27
Klaus Kämpf <kkaempf@suse.de>
2009-05-08 15:07:13 +0200

    Move the formatting out of the controller into views

--- plugins/commandline/app/controllers/yast/commandlines_controller.rb
+++ plugins/commandline/app/controllers/yast/commandlines_controller.rb
@@ -4,150 +4,91 @@ class Yast::CommandlinesController < ApplicationController
   before_filter :login_required
 
   require 'commandline'
-  
+
   ################################
   private
 
   def init_modules (check_policy)
+    return unless (!check_policy or permission_check( "org.opensuse.yast.commandline.read"))
     @yast_modules = Hash.new
-    if (!check_policy or
-        permission_check( "org.opensuse.yast.commandline.read"))
-      Commandline.all.each do |d|
-	begin
-	  yast_module = Commandline.new d
-	  @yast_modules[yast_module.id] = yast_module
-	rescue # Don't fail on non-existing service. Should be more specific.
-	end
+    Commandline.all.each do |d|
+      begin
+	yast_module = Commandline.new d
+	@yast_modules[yast_module.id] = yast_module
+      rescue # Don't fail on non-existing service. Should be more specific.
       end
-    else
-      yast_module = Commandline.new
-      yast_module.error_id = 1
-      yast_module.error_string = "no permission"
-      @yast_modules[yast_module.id] = yast_module
     end
   end
 
-  def respond data
-    if data
-      respond_to do |format|
-	format.xml do
-	  render :xml => data.to_xml
-	end
-	format.json do
-	  render :json => data.to_json
-	end
-	format.html do
-	  render :xml => data.to_xml #return xml only
-	end
-      end
-    else
-      render :nothing => true, :status => 404 unless @yast_modules # not found
-    end
-  end
-  
-  
   ################################
   public
 
   def index
     init_modules true #check policy
-    respond @yast_modules
+    if @yast_modules.nil?
+      render ErrorResult.error(403, 1, "no permission") and return
+    end
   end
 
   def show
-    id = params[:id]
-    if permission_check( "org.opensuse.yast.commandline.read") 
-       init_modules false #check no policy
-       @yast_module = @yast_modules[id]
-       @yast_module.commands #evaluate commands
-    else
-       @yast_module = Commandline.new ""
-       @yast_module.error_id = 1
-       @yast_module.error_string = "no permission"
+    unless permission_check( "org.opensuse.yast.commandline.read") 
+      render ErrorResult.error(403, 1, "no permission") and return
     end
-
-    respond @yast_module
+      
+    id = params[:id]
+    init_modules false #check no policy
+    @yast_module = @yast_modules[id]
+    @yast_module.commands #evaluate commands
   end
 
   # Hide running a command behind HTTP POST (aka create)
   def create
+    unless permission_check( "org.opensuse.yast.commandline.execute")
+      render ErrorResult.error(403, 1, "no permission") and return
+    end
     id = params[:id]
     @cmd_ret = Hash.new
-    if permission_check( "org.opensuse.yast.commandline.execute")
-       init_modules false #check no policy
-       @yast_module = @yast_modules[id]
-       @yast_module.commands #evaluate commands
+    init_modules false #check no policy
+    @yast_module = @yast_modules[id]
+    @yast_module.commands #evaluate commands
 
-       if request.post?
-         #execute command
 
-         if params["hash"] != nil
-           #checking if the command is hosted in a own Hash
-           params["hash"].each do |name,value|
-              params[name] = value
-           end
-         end
-      
-         cmd = ["/sbin/yast2", params[:id], params[:command]]
-         found = false
-         @yast_module.commands.each do |cname,option|
-           if cname == params["command"]
-             found = true
-             command = @yast_module.commands[cname]
-             if command != nil
-               command["options"].each do |name,option|
-                 if params[name.to_s] != nil
-                   if option["type"] == "string" 
-                     if !params[name.to_s].empty? 
-                       cmd << "#{name}=\"#{params[name.to_s]}\""
-                     end
-                   else 
-                     if option["type"] == nil
-                       cmd << name
-                     else
-                        cmd << "#{name}=#{params[name.to_s]}"
-                     end
-                   end
-                 end
-               end
-             end
-	     require "scr"
-             @cmd_ret = Scr.instance.execute(cmd)
-           end
-         end
-         if !found
-           Rails.logger.error "command #{params[:command]} not found"
-           @cmd_ret["exit"] = 2
-           @cmd_ret["stderr"] = "command #{params[:command]} not found"
-           @cmd_ret["stdout"] = ""
-         end
-       else
-         # no POST request
-         if permission_check( "org.opensuse.yast.commandline.read") 
-            respond @yast_module
-            return
-         else
-            @cmd_ret[:exit] = 1
-            @cmd_ret[:stderr] = "no permission"
-            @cmd_ret[:stdout] = ""
-         end
-       end
-    else #no right
-       @cmd_ret[:exit] = 1
-       @cmd_ret[:stderr] = "no permission"
-       @cmd_ret[:stdout] = ""
+    if params["hash"] != nil
+      #checking if the command is hosted in a own Hash
+      params["hash"].each do |name,value|
+	params[name] = value
+      end
     end
-
-    respond_to do |format|
-       format.xml do
-          render :xml => @cmd_ret.to_xml
-       end
-       format.json do
-          render :json => @cmd_ret.to_json
-       end
-       format.html do
-          render :xml => @cmd_ret.to_xml #return xml only
-       end
+    
+    cmd = ["/sbin/yast2", params[:id], params[:command]]
+    found = false
+    @yast_module.commands.each do |cname,option|
+      if cname == params["command"]
+	found = true
+	command = @yast_module.commands[cname]
+	if command != nil
+	  command["options"].each do |name,option|
+	    if params[name.to_s] != nil
+	      if option["type"] == "string" 
+		if !params[name.to_s].empty? 
+		  cmd << "#{name}=\"#{params[name.to_s]}\""
+		end
+	      else 
+		if option["type"] == nil
+		  cmd << name
+		else
+		  cmd << "#{name}=#{params[name.to_s]}"
+		end
+	      end
+	    end
+	  end
+	end
+	require "scr"
+	@cmd_ret = Scr.instance.execute(cmd)
+      end
+    end
+    if !found
+      render ErrorResult.error(404, 2, "command #{params[:command]} not found") and return
     end
   end
 end
