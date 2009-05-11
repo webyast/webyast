7a0a568869d4811c6ce23ae2528a937e665a5250
Stefan Schubert <schubi@suse.de>
2009-05-11 10:37:38 +0200

    taking default value

--- plugins/commandline/test/test_helper.rb
+++ plugins/commandline/test/test_helper.rb
@@ -1,7 +1,12 @@
 rails_parent = ENV["RAILS_PARENT"]
 unless rails_parent
-  $stderr.puts "Please set RAILS_PARENT environment"
-  exit
+  if File.directory?("../../webservice/")
+     $stderr.puts "Taking ../../webservice/ for RAILS_PARENT"  
+     rails_parent="../../webservice/"
+  else
+     $stderr.puts "Please set RAILS_PARENT environment"
+     exit
+  end
 end
 # first config rails
 require File.expand_path(rails_parent + "/config/environment")
