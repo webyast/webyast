b4fcc26896175216171b7b8c9de0df58126da2e6
Stefan Schubert <schubi@suse.de>
2009-05-15 12:28:06 +0200

    fixes for patches

--- plugins/patches/app/models/patch.rb
+++ plugins/patches/app/models/patch.rb
@@ -9,6 +9,14 @@ class Patch
                   :error_id,
                   :error_string
 
+  def id
+    @resolvable_id
+  end
+
+  def id=(id_val)
+    @resolvable_id = id_val
+  end
+
   def initialize 
      @error_id = 0
      @error_string = ""
@@ -19,6 +27,7 @@ class Patch
     xml.instruct! unless options[:skip_instruct]
 
     xml.patch_update do
+      xml.tag!(:id, id )
       xml.tag!(:resolvable_id, @resolvable_id, {:type => "integer"} )
       xml.tag!(:kind, @kind )
       xml.tag!(:name, @name )
--- plugins/patches/config/routes.rb
+++ plugins/patches/config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.resources :patch_updates
-  map.connect "/patch_updates/:id", :controller => 'patch_updates', :action => 'install'
 
 end
