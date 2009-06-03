c4bdf84948fd53fd2a699f6d6c826de6b65e4749
bgeuken <bgeuken@suse.de>
2009-06-03 12:31:54 +0200

    security module: fixed tests

--- plugins/securities/test/functional/security_controller_test.rb
+++ plugins/securities/test/functional/security_controller_test.rb
@@ -8,44 +8,53 @@ class SecurityControllerTest < ActionController::TestCase
     # http://railsforum.com/viewtopic.php?id=1719
   end
 
+  def test_actions_not_logged_in
+#    actions = %w{:show :create :update} #index?
+#    actions.each do |ac|
+    get :show
+    assert_response :unauthorized
+    assert_equal nil, flash[:notice]
+    get :update
+    assert_response :unauthorized
+    assert_equal nil, flash[:notice]
+    get :create
+    assert_response :unauthorized
+    assert_equal nil, flash[:notice]
+#   end
+  end
+
   def test_show # must run as root, uses scr.execute
     @request.session[:account_id] = 1 # defined in fixtures
     get :show
     assert_response :success
   end
 
-  def test_update # must run as root, uses scr.execute
-    @request.session[:account_id] = 1 # defined in fixtures
+  def test_update_with_params # must run as root, uses scr.execute
+    @request.session[:account_id] = 1
     get :update, :security => {:firewall => true, :firewall_after_startup => true, :ssh => true}
+    assert_response :success
   end
 
-  def test_actions_not_logged_in
-    actions = %w{show create update} #index?
-    actions.each do |ac|
-      get ac
-      assert_response :unauthorized
-      assert_equal nil, flash[:notice]
-   end
+  def test_update_without_params
+    @request.session[:account_id] = 1
+    get :update
+    assert_response 404, "update without params should not succeed"
   end
 
-=begin
-  def test_xml_response
-    test "access show xml" do
-      mime = Mime::XML
-      @request.accept = mime.to_s
-      get :show, :format => :xml
-      assert_equal mime.to_s, @response.content_type
-    end
+  def test_show_xml
+    @request.session[:account_id] = 1
+    mime = Mime::XML
+    @request.accept = mime.to_s
+    get :show, :format => :xml
+    assert_equal mime.to_s, @response.content_type
   end
 
-  def test_json_response
-    test "access show json" do
-      mime = Mime::JSON
-      @request.accept = mime.to_s
-      get :show, :format => :json
-      assert_equal mime.to_s, @response.content_type
-    end
+  def test_show_json
+    @request.session[:account_id] = 1
+    mime = Mime::JSON
+    @request.accept = mime.to_s
+    get :show, :format => :json
+    assert_equal mime.to_s, @response.content_type
   end
-=end
 end
 
