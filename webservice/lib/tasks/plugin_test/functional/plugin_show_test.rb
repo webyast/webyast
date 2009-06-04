a4ff6dda879052b9ebbc4fe71ff5036e787ee631
Stefan Schubert <schubi@suse.de>
2009-06-04 15:47:42 +0200

    added plugin tests in system_check

---
+++ webservice/lib/tasks/plugin_test/functional/plugin_show_test.rb
@@ -0,0 +1,47 @@
+require 'fileutils'
+require 'getoptlong'
+
+options = GetoptLong.new(
+  [ "--plugin",   GetoptLong::REQUIRED_ARGUMENT ]
+)
+
+$pluginname = nil
+
+begin
+options.each do |opt, arg|
+  case opt
+    when "--plugin": $pluginname = arg
+    else
+	STDERR.puts "Ignoring unrecognized option #{opt}"
+  end
+end
+rescue
+end
+
+
+require File.expand_path(File.dirname(__FILE__) + "/../test_helper")
+require 'test/unit'
+require 'rubygems'
+require "scr"
+
+def normalize_name name
+    name.split("_").collect { |i| i.capitalize }.join
+end
+
+class PluginIndexTest < ActionController::TestCase
+  fixtures :accounts
+  def setup
+    classname = normalize_name $pluginname
+    puts "Checking #{classname}"
+    @controller = Kernel.const_get( classname ).new
+    @request = ActionController::TestRequest.new
+    # http://railsforum.com/viewtopic.php?id=1719
+    @request.session[:account_id] = 1 # defined in fixtures
+  end
+  
+  test "access show" do
+    get :show
+    assert_response :success
+  end
+
+end
---
+++ webservice/lib/tasks/plugin_test/test_helper.rb
@@ -0,0 +1,60 @@
+rails_parent = ENV["RAILS_PARENT"]
+unless rails_parent
+  if File.directory?("../../webservice/")
+     $stderr.puts "Taking ../../webservice/ for RAILS_PARENT"  
+     rails_parent="../../webservice/"
+  else
+     $stderr.puts "Please set RAILS_PARENT environment"
+     exit
+  end
+end
+# first config rails
+require File.expand_path(rails_parent + "/config/environment")
+# then enable testing, this will get the routing right
+ENV["RAILS_ENV"] = "test"
+require 'test_help'
+
+class ActiveSupport::TestCase
+  # Transactional fixtures accelerate your tests by wrapping each test method
+  # in a transaction that's rolled back on completion.  This ensures that the
+  # test database remains unchanged so your fixtures don't have to be reloaded
+  # between every test method.  Fewer database queries means faster tests.
+  #
+  # Read Mike Clark's excellent walkthrough at
+  #   http://clarkware.com/cgi/blosxom/2005/10/24#Rails10FastTesting
+  #
+  # Every Active Record database supports transactions except MyISAM tables
+  # in MySQL.  Turn off transactional fixtures in this case; however, if you
+  # don't care one way or the other, switching from MyISAM to InnoDB tables
+  # is recommended.
+  #
+  # The only drawback to using transactional fixtures is when you actually 
+  # need to test transactions.  Since your test is bracketed by a transaction,
+  # any transactions started in your code will be automatically rolled back.
+  self.use_transactional_fixtures = true
+
+  # Instantiated fixtures are slow, but give you @david where otherwise you
+  # would need people(:david).  If you don't want to migrate your existing
+  # test cases which use the @david style and don't mind the speed hit (each
+  # instantiated fixtures translates to a database query per test method),
+  # then set this back to true.
+  self.use_instantiated_fixtures  = false
+
+  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+  
+  # See http://pennysmalls.com/2009/03/04/rails-23-breakage-and-fixage/
+  def clean_backtrace(&block)
+    yield
+  rescue ActiveSupport::TestCase::Assertion => error
+    framework_path = Regexp.new(File.expand_path("#{File.dirname(__FILE__)}/assertions"))
+    error.backtrace.reject! { |line| File.expand_path(line) =~ framework_path }
+    raise
+  end
+		  
+end
