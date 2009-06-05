2ef20ef9abd4c5960a3f90872b68a5f5fb7c53b3
Stefan Schubert <schubi@suse.de>
2009-06-05 15:49:10 +0200

    added samba testcase; fixed general testcases concerning module names

---
+++ plugins/samba_server/test/functional/sambashares_controller_test.rb
@@ -0,0 +1,23 @@
+require File.expand_path(File.dirname(__FILE__) + "/../test_helper")
+require 'test/unit'
+require 'rubygems'
+require "scr"
+require 'mocha'
+
+
+class SambasharesControllerTest < ActionController::TestCase
+  fixtures :accounts
+  def setup
+    @controller = SambasharesController.new
+    @request = ActionController::TestRequest.new
+    # http://railsforum.com/viewtopic.php?id=1719
+    @request.session[:account_id] = 1 # defined in fixtures
+  end
+  
+  test "access index" do
+    get :show
+    assert_response :success
+  end
+
+
+end
