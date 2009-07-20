dafeb0303d9c56ca7ccdf9b857e10542e69d0c8a
Duncan Mac-Vicar P <dmacvicar@suse.de>
2009-07-20 17:03:40 +0200

    require status class

--- plugins/status/test/unit/status_test.rb
+++ plugins/status/test/unit/status_test.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + "/../test_helper")
 require 'scr'
 require 'mocha'
+require 'status'
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
