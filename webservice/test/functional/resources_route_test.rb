e439593bd81d1e7da6d0864b900ff4c75e259c03
Josef Reidinger <jreidinger@suse.cz>
2009-08-18 15:27:53 +0200

    add general test for plugins private routing

--- webservice/test/functional/resources_route_test.rb
+++ webservice/test/functional/resources_route_test.rb
@@ -45,5 +45,14 @@ class ResourceRouteTest < ActiveSupport::TestCase
       end
     end
   end
+
+  test "plugin private routing" do
+    searchdir = File.join(File.dirname(__FILE__),"..","..","..","plugins")
+    Dir.foreach(searchdir) { |filename|
+      unless filename[0].chr == "."
+        assert !File.exist?(File.join(searchdir,filename,"config","routes.rb")), "Plugin #{filename} contains private routing"
+      end
+    }
+  end
   
 end
