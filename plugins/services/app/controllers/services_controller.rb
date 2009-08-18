4fe7bc0cfc38df7ad9a15db2f10601ae3778638c
Jiri Suchomel <jsuchome@suse.cz>
2009-08-18 15:20:38 +0200

    back to YaPI based version of services

--- plugins/services/app/controllers/services_controller.rb
+++ plugins/services/app/controllers/services_controller.rb
@@ -1,30 +1,54 @@
+require 'service'
+
 class ServicesController < ApplicationController
   before_filter :login_required
 
   def index
-    unless permission_check( "org.opensuse.yast.system.services.read")
+    unless permission_check("org.opensuse.yast.modules.yapi.services.read")
       render ErrorResult.error(403, 1, "no permission") and return
     end
-
-    @services = Lsbservice.all
-    @services = @services.sort unless params[:sort] == "0"
-
-    @services = @services.map {|svc| {:link => svc} }
+    @services	= Service.find_all
   end
 
-  # show the svc including the commands
+  # Shows service status. Requires read permission for services YaPI.
   def show
-    unless permission_check( "org.opensuse.yast.system.services.read")
-      render ErrorResult.error(403, 1, "no permission") and return
+    unless permission_check("org.opensuse.yast.modules.yapi.services.get")
+      render ErrorResult.error( 403, 1, "no permission" ) and return
     end
-    id = params[:id]
 
+    id = params[:id]
     begin
-      @service = Lsbservice.new id
+      @service = Service.find id
     rescue Exception => e # Don't fail on non-existing service. Should be more specific.
       logger.debug e
       render ErrorResult.error(404, 106, "no such service") and return
     end
     logger.debug "show@service #{@service.inspect}"
   end
+
+  # PUT /services/1.xml
+  def update
+
+    unless permission_check( "org.opensuse.yast.modules.yapi.services.execute")
+      render ErrorResult.error(403, 1, "no permission") and return
+    end
+
+    begin
+      @service = Service.find params[:id]
+    rescue Exception => e
+      logger.debug e
+      render ErrorResult.error(404, 106, "no such service") and return
+    end
+
+    ret	= {}
+    begin
+      ret	= @service.save(params[:execute])
+    rescue Exception => e
+      logger.debug e
+      render ErrorResult.error(404, @error_id, @error_string) and return
+    end
+
+    render :xml => ret
+  end
+
 end
--- plugins/services/test/functional/services_controller_test.rb
+++ plugins/services/test/functional/services_controller_test.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require "scr"
 require 'mocha'
 
+
 class ServicesControllerTest < ActionController::TestCase
   fixtures :accounts
   def setup
@@ -12,15 +13,16 @@ class ServicesControllerTest < ActionController::TestCase
     # http://railsforum.com/viewtopic.php?id=1719
     @request.session[:account_id] = 1 # defined in fixtures
 
-    s1 = Lsbservice.new("foo")
-    s1.stubs(:path).returns("/foo")
-    s1.stubs(:commands).returns(["start", "stop"])
+    s1 = Service.new
+    s1.name = "foo"
+    s1.status = 0
 
-    s2 = Lsbservice.new("cron")
-    s2.stubs(:path).returns("/cron")
-    s2.stubs(:commands).returns(["start", "stop", "kill"])
-
-    Lsbservice.stubs(:all).returns([s1, s2])
+    s2 = Service.new
+    s2.name = "cron"
+    s2.status = 1
+    
+    Service.stubs(:find_all).returns([s1, s2])
+    Service.stubs(:find).with("cron").returns([s2])
   end
   
   test "access index" do
