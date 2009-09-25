826d88a280dbb17d657f3fd765aa336d6f1c9a28
Klaus Kämpf <kkaempf@suse.de>
2009-09-25 18:03:46 +0200

    Full SearchName test for PackageKit/D-Bus
    This uses PackageKitStub and PackageKitResult to test the 'execute'
    method of app/models/resolvables.

--- plugins/patches/test/unit/resolvable_test.rb
+++ plugins/patches/test/unit/resolvable_test.rb
@@ -12,16 +12,25 @@ class ResolvableTest < ActiveSupport::TestCase
   require 'resolvable'
 
   def setup
-    PackageKitStub.stub!
+    @pk_stub = PackageKitStub.new
   end
 
+  # (dummy) test 'SearchName'
+  # this mostly tests correct stubbing
   def test_resolvable_search
-    Resolvable.execute( "SearchName", ["installed;~devel", "yast2"], "Package") do |info,id,summary|
-      assert info
-      assert id
-      assert summary
+    results = Array.new
+    results << PackageKitResult.new( "info1", "id1", "summary1" )
+    results << PackageKitResult.new( "info2", "id2", "summary2" )
+    signal = "Package"
+    @pk_stub.run signal, results
+    count = 0
+    Resolvable.execute( "SearchName", ["installed;~devel", "yast2"], signal) do |info,id,summary|
+      assert_equal results[count].info, info
+      assert_equal results[count].id, id
+      assert_equal results[count].summary, summary
+      count += 1
     end
-    
+    assert_equal results.size, count
   end
   
 end
