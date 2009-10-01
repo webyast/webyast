8474654f7d20f89b42c47b478ce6678a0c183f22
Michal Zugec <mzugec@urchin.suse.cz>
2009-10-01 17:41:37 +0200

    test to raise exception when invalid default route

--- plugins/network/test/functional/routes_controller_test.rb
+++ plugins/network/test/functional/routes_controller_test.rb
@@ -34,5 +34,18 @@ class RoutesControllerTest < ActionController::TestCase
     assert_equal "default",   h["routes"][0]["id"]
   end
 
+  DATA={"routes"=>{
+       "id"=>"default",
+       "via"=>"10.20.30"
+	 },
+      "id"=>"default"
+     }
+
+  def test_validation
+    put :update, DATA
+    h = Hash.from_xml @response.body
+    assert_equal "NETWORK_ROUTE_ERROR", h["error"]["type"]
+  end
+
 end
 
