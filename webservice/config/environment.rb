aa4e74a3d8aa7de1d1e6cdc26ea2383c3c34adf6
Klaus Kämpf <kkaempf@suse.de>
2009-10-28 11:30:07 +0100

    add fixme and link to bnc#

--- webservice/config/environment.rb
+++ webservice/config/environment.rb
@@ -58,6 +58,7 @@ init = Rails::Initializer.run do |config|
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
     :session_key => '_yast-api_session',
+    # FIXME: should be generated at package install time (bnc#550635)
     :secret      => '9d11bfc98abcf9799082d9c34ec94dc1cc926f0f1bf4bea8c440b497d96b14c1f712c8784d0303ee7dd69e382c3e5e4d38d4c56d1b619eae7acaa6516cd733b1'
   }
 
