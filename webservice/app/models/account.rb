cf08e5394127c59a82aa65c19df70402483490fc
Klaus Kämpf <kkaempf@suse.de>
2009-10-28 12:23:23 +0100

    comment typo

--- webservice/app/models/account.rb
+++ webservice/app/models/account.rb
@@ -27,7 +27,7 @@ class Account < ActiveRecord::Base
      return false if login.match "'" or login.match /\\$/ #don't allow ' or \ in login to prevent security issues
      cmd = "/sbin/unix2_chkpwd rpam '#{login}'"
      se = Session.new
-     result, err = se.execute cmd, :stdin => passwd #password needn't pt be escaped as it is on stdin
+     result, err = se.execute cmd, :stdin => passwd #password needn't to be escaped as it is on stdin
      return (se.get_status == 0)
   end
 
