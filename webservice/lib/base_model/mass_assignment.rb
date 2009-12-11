1cb47ff74e910a0509c235c6d296db42504912eb
Josef Reidinger <jreidinger@suse.cz>
2009-12-11 18:15:22 +0100

    add documentation to BaseModel (partial only)

--- webservice/lib/base_model/mass_assignment.rb
+++ webservice/lib/base_model/mass_assignment.rb
@@ -1,5 +1,28 @@
 module BaseModel
+  # == Mass Assignment module
+  # Adds ability to load instance variables from hash
+  # It allow whitelisting and blacklisting variables which should not be
+  # overwritten. It respect common behavior in ActiveResource and ActiveRecord.
+  # For mass loading see ActiveResource::Base#load
+  # For whitelisting details see ActiveRecord::Base.attr_accessible
+  # For blacklisting details see ActiveRecord::Base.attr_protected
+  #
+  # Example:
+  #   class C
+  #     include BaseModel::MassAssignment
+  #
+  #     attr_accessor :attr1, attr2, :internal
+  #
+  #     attr_protected :internal
+  #
+  #     def instantiate(attr={})
+  #         load attr
+  #     end
+  #   end
   module MassAssignment
+    # loads hash to instance variables, where keys is names of variables and 
+    # hash value is variables values
+    # it has same behavior as ActiveResource::Base#load
     def load(attributes)
       attributes.each do |k,v|
         whitelist = self.class.accessible_attributes
@@ -15,20 +38,33 @@ module BaseModel
     end
 
     module ClassMethods
+
+      # Defines attributes which should be loaded by mass assignment.
+      # By default is used all attributes
+      # For disable using loading non-whitelisted attributes use
+      # without parameters
+      # param args is variable number of symbols which identify variables
       def attr_accessible ( *args )
         @attr_accessible ||= []
         @attr_accessible.concat args
       end
 
+      # Gets list of allowed attributes. If result is nil, then is allowed is
+      # all attributes which is not protected.
       def accessible_attributes
         @attr_accessible
       end
 
+      # Defines attributes which cannot be loaded by mass assignment.
+      # By default all attributes is not protected.
+      # Note only used if whitelist is not specified by attr_accessible
+      # param args is variable number of symbols which identify variables
       def attr_protected ( *args )
         @attr_protected ||= []
         @attr_protected.concat args
       end
 
+      # Gets list of protected attributes. If result is nil, then no attributes is protected.
       def protected_attributes
         @attr_protected
       end
