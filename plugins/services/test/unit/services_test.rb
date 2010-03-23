6bdb08dca182841f8894735fe650b1ae0d850d48
Jiri Suchomel <jsuchome@suse.cz>
2010-03-23 16:10:52 +0100

    added fake filter file

--- plugins/services/test/unit/services_test.rb
+++ plugins/services/test/unit/services_test.rb
@@ -12,6 +12,8 @@ services:
 EOF
 
   def setup    
+    Paths.const_set 'CONFIG', File.join(File.dirname(__FILE__),"..","..","test","etc")
+    YaST::ConfigFile.any_instance.stubs(:path).returns(__FILE__)
     YaST::ConfigFile.stubs(:read_file).returns(YAML_CONTENT)
     @read_args = {
 	'runlevel'		=> [ 'i', 5 ],
