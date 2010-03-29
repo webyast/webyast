dfabc35c8527dcc985569daefd614a1da928c63c
Stefan Schubert <schubi@suse.de>
2010-03-29 14:56:26 +0200

    mechanism for status-plugin communication

--- plugins/registration/app/models/registration_state.rb
+++ plugins/registration/app/models/registration_state.rb
@@ -1,13 +1,16 @@
 
 class RegistrationState
   def self.read()
-#    return { :level => "warning",
-#             :message_id => "MISSING_REGISTRATION",
-#             :short_description => "Registration is missing",
-#             :long_description => "Please register your system in order to get updates.",
-#             :confirmation_host => "client",
-#             :confirmation_link => "/registration",
-#             :confirmation_label => "register" }
-     return {}
+    unless Register.new.is_registered?
+      return { :level => "warning",
+               :message_id => "MISSING_REGISTRATION",
+               :short_description => "Registration is missing",
+               :long_description => "Please register your system in order to get updates.",
+               :confirmation_host => "client",
+               :confirmation_link => "/registration",
+               :confirmation_label => "register" }
+     else
+       return {}
+     end
   end
 end
