36c0189eb4e508d597979e1fa39bee03fae3c262
Jiri Suchomel <jsuchome@suse.cz>
2010-03-29 16:31:04 +0200

    - enhanced confirmation messages (bnc#580458) - 0.1.11

--- plugins/mail/app/models/mail_state.rb
+++ plugins/mail/app/models/mail_state.rb
@@ -2,13 +2,20 @@
 class MailState
   def self.read()
     if File.exist? Mail::TEST_MAIL_FILE
+      f = File.new(Mail::TEST_MAIL_FILE, 'r')
+      mail = f.gets.chomp
+      mail = "" if mail.nil?
+      f.close
       return { :level => "warning",
                :message_id => "MAIL_SENT",
-               :short_description => "Test mail status",
-               :long_description => "Do you have received your test mail ?",
+               :short_description => "Mail configuration test not confirmed",
+               :long_description => "During Mail configuration, test mail was sent to '%s'.
+Was the mail delivered to this address?<br>
+If so, confirm it by pressing the button. Otherwise, check your mail confiuration again, even the '/var/log/mail' file." % mail,
                :confirmation_host => "service",
                :confirmation_link => "/mail/state",
-               :confirmation_label => "Received" }
+               :confirmation_label => "Test mail received" }
+      # TODO what about passing :log_file => '/var/log/mail', so status page could show its content?
     else
       return {}
     end   
