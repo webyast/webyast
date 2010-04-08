7802b61e23751e0d07b8dde4bb311dc0397eac40
Josef Reidinger <jreidinger@suse.cz>
2010-04-08 15:28:01 +0200

    allow edit ntp server

--- plugins/ntp/app/models/ntp.rb
+++ plugins/ntp/app/models/ntp.rb
@@ -9,9 +9,7 @@ class Ntp < BaseModel::Base
       ret.actions ||= {}
       ret.actions[:synchronize] = false
       ret.actions[:synchronize_utc] = true
-      unless YastService.Call("YaPI::NTP::Available")
-        ret.actions[:ntp_server] = ""
-      end
+      ret.actions[:ntp_server] = get_servers_string
       return ret
     end
 
@@ -19,8 +17,20 @@ class Ntp < BaseModel::Base
       synchronize if @actions[:synchronize]
     end
 
+    def self.get_servers
+      get_servers_string
+    end
+
   private
+    
+    def self.get_servers_string
+      ret = `grep "^[:space:]*NETCONFIG_NTP_STATIC_SERVERS" /etc/sysconfig/network/config | sed 's/.*="\\(.*\\)"/\\1/'`
+      Rails.logger.info "greped server list is #{ret}"
+      ret
+    end
+    
     def synchronize
+      @actions.delete :ntp_server if @actions[:ntp_server] == Ntp.get_servers
       ret = "OK"
       begin
         ret = YastService.Call("YaPI::NTP::Synchronize",@actions[:synchronize_utc],@actions[:ntp_server]||"")
