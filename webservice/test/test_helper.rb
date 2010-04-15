65b68feb0b23d85e7843f082d85250124edd214e
Ladislav Slezak <lslezak@novell.com>
2010-04-15 15:42:42 +0200

    move the common code from lib/test/override_test_db.rb to the shared test_helper.rb

--- webservice/test/test_helper.rb
+++ webservice/test/test_helper.rb
@@ -52,5 +52,20 @@ class ActiveSupport::TestCase
     error.backtrace.reject! { |line| File.expand_path(line) =~ framework_path }
     raise
   end
-		  
+
 end
+
+# use a different DB for tests -  needed during RPM build
+if !ENV['TEST_DB_PATH'].nil? && ENV['RAILS_ENV'] == 'test'
+  Rails.logger.debug "Using DB file for tests: #{ENV['TEST_DB_PATH']}"
+
+  # read the current config
+  dbconf = Rails::Configuration.new.database_configuration['test']
+
+  # update the config and make a new DB connection
+  require 'active_record'
+  dbconf['database'] = ENV['TEST_DB_PATH']
+  ActiveRecord::Base.establish_connection(dbconf)
+  ActiveRecord::Base.connection
+end
+
