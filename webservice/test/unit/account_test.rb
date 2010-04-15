17391f2c93a9187c0fc03f452e0a5dfa2214205b
Klaus Kämpf <kkaempf@suse.de>
2010-04-15 11:14:24 +0200

    Further tests for Account model

--- webservice/test/unit/account_test.rb
+++ webservice/test/unit/account_test.rb
@@ -7,9 +7,8 @@ class AccountTest < ActiveSupport::TestCase
   fixtures :accounts
   
   def setup
-    # see lib/session.rb, used by app/models/account.rb
-    Session::Sh.any_instance.stubs(:execute).returns(nil)
-    Session::Sh.any_instance.stubs(:get_status).returns(0)
+    @login = "test_user"
+    @passwd = "secret"
   end
   
   test "bad character in login" do
@@ -18,11 +17,52 @@ class AccountTest < ActiveSupport::TestCase
   end
   
   test "unix2_chkpwd" do
-    @login = "test_user"
     @cmd = "/sbin/unix2_chkpwd rpam '#{@login}'"
-    @passwd = "secret"
-    Session::Sh.any_instance.stubs(:execute).with(){ |cmd,hash| assert_equal @cmd, cmd ; assert_equal @passwd, hash[:stdin] }.returns(nil)
+    Session::Sh.any_instance.stubs(:execute).with(@cmd, { :stdin => @passwd }).returns(nil)
     Session::Sh.any_instance.stubs(:get_status).returns(0)
     assert Account.unix2_chkpwd(@login, @passwd)
   end
+  
+  test "authenticate with rpam" do
+    Rpam.expects(:authpam).with(@login, @passwd).returns(true)
+    Account.expects(:unix2_chkpwd).never # ensure chkpwd isn't called
+    assert Account.authenticate( @login, @passwd )
+  end
+  
+  test "authenticate with chkpwd" do
+    Rpam.expects(:authpam).with(@login, @passwd).returns(false)
+    Account.expects(:unix2_chkpwd).once.returns(true) # ensure chkpwd is called
+    assert Account.authenticate( @login, @passwd )
+  end
+  
+  test "failed authenticate with chkpwd" do
+    Rpam.expects(:authpam).with(@login, @passwd).returns(false)
+    Account.expects(:unix2_chkpwd).once.returns(false) # ensure chkpwd is called
+    assert !Account.authenticate( @login, @passwd )
+  end
+  
+  test "authenticate saves password" do
+    Rpam.expects(:authpam).with(@login, @passwd).returns(false)
+    Account.expects(:unix2_chkpwd).once.returns(true)
+    acc = Account.authenticate( @login, @passwd )
+    assert acc
+    assert_equal @login, acc.login
+    assert_equal @passwd, acc.password
+  end
+  
+  test "class encrypt" do
+    s = Account.encrypt "data", "salt"
+    assert s.is_a? String
+    assert_equal 40, s.length # SHA1
+  end
+  
+  test "instance encrypt" do
+    Rpam.expects(:authpam).with(@login, @passwd).returns(false)
+    Account.expects(:unix2_chkpwd).once.returns(true)
+    acc = Account.authenticate( @login, @passwd )
+    assert acc
+    s = Account.encrypt "data", acc.salt
+    assert_equal s, acc.encrypt("data")
+  end
+  
 end
