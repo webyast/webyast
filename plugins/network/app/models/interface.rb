84e0e46d63a5ce1356439da5ccaebf1f529df4fa
Josef Reidinger <jreidinger@suse.cz>
2010-04-20 12:06:08 +0200

    add validations for interface model

--- plugins/network/app/models/interface.rb
+++ plugins/network/app/models/interface.rb
@@ -22,20 +22,22 @@
 # Provides set and gets resources from YaPI network module.
 # Main goal is handle YaPI specific calls and data formats. Provides cleaned
 # and well defined data.
-class Interface
+class Interface < BaseModel::Base
 
-  attr_accessor :bootproto,
-  		:ipaddr,
-		:id
-
-  private
+  attr_accessor :bootproto
+  validates_inclusion_of :bootproto, :in => ["static","dhcp"]
+  attr_accessor :ipaddr
+  validates_format_of :ipaddr, :allow_nil => true,
+      :with => /^([0-9]{1,3}.){3}[0-9]{1,3}\/[0-9]{1,2}$/
+  attr_accessor	:id
+  validates_format_of :id, :allow_nil => false,
+      :with => /^[a-zA-Z0-9_-]+$/
 
   public
 
-  def initialize(kwargs, id=nil)
-    @bootproto = kwargs["bootproto"]
-    @ipaddr    = kwargs["ipaddr"] || ""
-    @id	       = kwargs["id"] || id
+  def initialize(args, id=nil)
+    super args
+    @id ||= id
   end
 
   def self.find( which )
@@ -55,7 +57,7 @@ class Interface
 
   # Saves data from model to system via YaPI. Saves only setted data,
   # so it support partial safe (e.g. save only new timezone if rest of fields is not set).
-  def save
+  def update
     if @bootproto==""
       settings = {@id=>{}}
     else
@@ -69,22 +71,6 @@ class Interface
     vsettings = [ "a{sa{ss}}", settings ] # bnc#538050
     YastService.Call("YaPI::NETWORK::Write",{"interface" => vsettings})
     # TODO success or not?
-  end
-
-  def to_xml( options = {} )
-    xml = options[:builder] ||= Builder::XmlMarkup.new(options)
-    xml.instruct! unless options[:skip_instruct]
-
-    xml.interface do
-      xml.id	@id
-      xml.bootproto @bootproto
-      xml.ipaddr    @ipaddr
-    end
-  end
-
-  def to_json( options = {} )
-    hash = Hash.from_xml(to_xml())
-    return hash.to_json
   end
 
 end
--- plugins/network/test/unit/interface_test.rb
+++ plugins/network/test/unit/interface_test.rb
@@ -18,12 +18,27 @@ class InterfaceTest < ActiveSupport::TestCase
  def test_getter1
    iface=Interface.find('eth0')
    assert_equal 'dhcp', iface.bootproto
+   assert_equal 'eth0', iface.id
  end
  
  def test_getter2
    iface=Interface.find('eth1')
    assert_equal 'static', iface.bootproto
    assert_equal '1.2.3.4/24', iface.ipaddr
+   assert_equal 'eth1', iface.id
+ end
+
+ def test_validations
+   iface = Interface.find('eth1')
+   assert iface.valid?
+   iface.bootproto = "dhcp6"
+   assert iface.invalid?
+   iface.bootproto = "static"
+   iface.id = "<script malicious>"
+   assert iface.invalid?
+   iface.id = "eth1"
+   iface.ipaddr = "<malicious>"
+   assert iface.invalid?
  end
 
 end
