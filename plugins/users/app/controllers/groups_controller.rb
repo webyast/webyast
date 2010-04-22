2a0ed693594f228870d1150df9e705b3ed3e416a
Klaus Kämpf <kkaempf@suse.de>
2010-04-22 15:40:19 +0200

    Catch and report DBus::Error generically (bnc#598794)

--- plugins/users/app/controllers/groups_controller.rb
+++ plugins/users/app/controllers/groups_controller.rb
@@ -40,19 +40,21 @@ private
     yapi_perm_check "users.groupadd"
   end
 
+  # log Group.find error and provide matching ErrorResult
+  def group_not_found gid
+    Rails.logger.error "Group #{gid} was not found."
+    ErrorResult.error(404, 2, "group #{gid} not found")
+  end
+  
 public
 
   # GET /groups/users
   # GET /groups/users.xml
   def show
-    begin
-      # try to find the grouplist, and 404 if it does not exist
-      @group = Group.find params[:id]
-      if @group.nil?
-        render ErrorResult.error(404, 2, "grouplist not found") and return
-      end
-    rescue Exception => e
-      render ErrorResult.error(500, 2, e.message) and return
+    # try to find the grouplist, and 404 if it does not exist
+    @group = Group.find params[:id]
+    if @group.nil?
+      render group_not_found(params[:id]) and return
     end
 
     respond_to do |format|
@@ -64,12 +66,10 @@ public
   # GET /groups.xml
   def index
     # read permissions were checked in a before filter
-    begin
-      @groups = Group.find_all
-    rescue DBus::Error => exception
-      render ErrorResult.error(404, 20, "DBus Error: #{exception.dbus_message.error_name}") and return
-    rescue Exception => e
-      render ErrorResult.error(500, 2, e.message) and return
+    @groups = Group.find_all
+    if @groups.nil?
+      Rails.logger.error "No groups found."
+      render ErrorResult.error(404, 2, "No groups not found") and return
     end
 
     respond_to do |format|
@@ -83,13 +83,9 @@ public
     group_params = params[:groups] || {}
     group_params[:old_cn] = params[:id]
     @group = Group.new group_params
-    begin
-      result = @group.save
-      unless result.empty?
-        render ErrorResult.error(404, 2, "Group update error:'"+result+"'") and return
-      end
-    rescue DBus::Error => exception
-        render ErrorResult.error(404, 20, "DBus Error: #{exception.dbus_message.error_name}") and return
+    result = @group.save
+    unless result.empty?
+      render ErrorResult.error(404, 2, "Group update error:'"+result+"'") and return
     end
     render :show
   end
@@ -99,13 +95,10 @@ public
     group_params = params[:groups] || {}
     group_params[:old_cn] = group_params[:cn]
     @group = Group.new group_params
-    begin
-      result = @group.save
-      unless result.empty?
-        render ErrorResult.error(404, 2, "Group create error:'"+result+"'") and return
-      end
-    rescue DBus::Error => exception
-        render ErrorResult.error(404, 20, "DBus Error: #{exception.dbus_message.error_name}") and return
+
+    result = @group.save
+    unless result.empty?
+      render ErrorResult.error(404, 2, "Group create error:'"+result+"'") and return
     end
     render :show
   end
@@ -114,27 +107,17 @@ public
   def destroy
     yapi_perm_check "users.groupdelete"
 
-    begin
-      @group = Group.find(params[:id])
-    rescue DBus::Error => exception
-        render ErrorResult.error(404, 20, "DBus Error: #{exception.dbus_message.error_name}") and return
-    end
+    @group = Group.find(params[:id])
 
     if @group.nil?
-      Rails.logger.error "Group #{params[:id]} was not found."
-      render ErrorResult.error(404, 1, "Group '#{params[:id]}' not found.") and return
+      render group_not_found(params[:id]) and return
     end
 
-    begin
-      result = @group.destroy
-      unless result.empty?
-        render ErrorResult.error(404, 2, "Cannot remove group #{@group.cn}: #{result}") and return
-      end
-    rescue DBus::Error => exception
-        render ErrorResult.error(404, 20, "DBus Error: #{exception.dbus_message.error_name}") and return
+    result = @group.destroy
+    unless result.empty?
+      render ErrorResult.error(404, 2, "Cannot remove group #{@group.cn}: #{result}") and return
     end
 
     render :show
   end
 end
-
