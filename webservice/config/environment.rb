f0be1e0240ba76c6c9a82816e535ae842a232f16
Josef Reidinger <jreidinger@suse.cz>
2010-06-01 15:47:23 +0200

    Revert "try specify rails version to debug problem on hudson"
    This reverts commit e8163f36e7a53a2fa64853e8d409c6dc50e4c56d.

--- webservice/config/environment.rb
+++ webservice/config/environment.rb
@@ -32,7 +32,7 @@ RAILS_ENV = ENV['RAILS_ENV'] unless defined? RAILS_ENV
 STDERR.puts "\n\n\t***RAILS_ENV environment variable isn't set !\n\n" unless RAILS_ENV
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+# RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
