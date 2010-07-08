05e919c6dee02dd3db0296dfb5949bbc83bf2b3b
Josef Reidinger <jreidinger@suse.cz>
2010-07-08 12:25:19 +0200

    add license header to permission service file

--- webservice/package/webyastPermissionsService.rb
+++ webservice/package/webyastPermissionsService.rb
@@ -1,4 +1,22 @@
 #!/usr/bin/env ruby
+#--
+# Webyast Webservice framework
+#
+# Copyright (C) 2009, 2010 Novell, Inc. 
+#   This library is free software; you can redistribute it and/or modify
+# it only under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation. 
+#
+#   This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+# details. 
+#
+#   You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software 
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#++
+
 
 require 'rubygems'
 require 'dbus'
@@ -23,7 +41,7 @@ class WebyastPermissionsService < DBus::Object
 
   def log(msg)
     f = File.new("/srv/www/yastws/log/permission_service.log","a",0600)
-    f.write (msg+"\n")
+    f.write(msg+"\n")
     f.close
   end
 
