6f2c9c42ad6ddd840a0edad64e34865b6e17fc3d
Vladislav Gorobets <vgorobets@suse.de>
2010-10-04 11:13:23 +0200

    - Missing text in tab headers (jquery.accordion) in Status module after update from WebYaST-SP1 to WebYaST 1.1 (bnc#636616) - webyast-status-ws 0.2.4

---
+++ plugins/status/lib/configcheck.rb
@@ -0,0 +1,56 @@
+require "yaml"
+CONFIGURATION_FILE = "status_configuration.yaml"
+KNOWN_KEYS = ['Network', 'CPU', 'Memory', 'Disk']
+PATH = File.join(File.dirname(__FILE__), '/../config/', CONFIGURATION_FILE)
+    
+def check_config()
+  if File.exist?(PATH)
+    hash = Hash.new
+    hash = YAML.load(File.open(PATH))
+
+    if node_exist?(hash)
+      #find headlines
+      unless subnode_exist?(hash, "headline")
+	add_headline(hash)
+
+	#check label translation
+	unless !subnode_exist?(hash, "y_label")
+	  check_label_translation(hash)
+	end 
+      end
+    end
+    #save changes
+    save_config(hash)
+  end
+end
+
+def node_exist?(hash)
+    KNOWN_KEYS.any? { |e| hash.has_key?(e) }
+end 
+
+def subnode_exist?(hash, string)
+  hash.any? { |key,subhash| subhash.has_key?(string) }
+end
+
+def add_headline(hash)
+  hash.each do |key, subhash|
+    subhash['headline'] = '_("' + key + '")' if KNOWN_KEYS.include?(key)
+  end
+end
+
+def check_label_translation(hash)
+  hash.each do |key, subhash|
+    subhash['y_label'] = '_("' + subhash["y_label"] + '")' unless subhash['y_label'] =~ /^_\(\"/ &&  subhash["y_label"] =~ /\"\)$/
+  end
+end
+
+def save_config(hash)
+  file_name = File.join(PATH)
+  File.open(file_name, "w") do |f|
+    f.write(hash.to_yaml)
+  end
+end
+
+#run config check
+check_config()
+
