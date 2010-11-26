6d2471b71bfa90647ec265600000c2e8f6366e53
mzugec <mzugec@suse.cz>
2010-11-26 11:45:47 +0100

    accept system users (bnc#638476)

--- plugins/users/test/unit/user_test.rb
+++ plugins/users/test/unit/user_test.rb
@@ -22,28 +22,26 @@
 require File.expand_path(File.dirname(__FILE__) + "/../test_helper")
 require 'test/unit'
 require "yast_service"
-require 'mocha'
-require 'pp'
+#require 'mocha'
+#require 'pp'
 
 class UserTest < Test::Unit::TestCase
 
   def setup
 
-    parameters	= { "index" => ["s", "uid"], "user_attributes" =>  
-        [ "as", [ "cn", "uidNumber", "homeDirectory",
-                  "grouplist", "uid", "loginShell", "groupname" ] ]
-    }
-    YastService.stubs(:Call).with("YaPI::USERS::UsersGet", parameters).returns({"testuser1"=>{"cn"=>"testuser1", "groupname"=>"users", "uidNumber"=>1000, "homeDirectory"=>"/home/testuser1"},
-										"testuser2"=>{"cn"=>"testuser2", "groupname"=>"users", "uidNumber"=>1010, "homeDirectory"=>"/home/testuser1"}})    
+    parameters = {'user_attributes' => ['as', ['cn', 'uidNumber', 'homeDirectory',
+          'grouplist', 'uid', 'loginShell', 'groupname']], 'type' => 'local', 'index' => ['s', 'uid']}
+    result = {"testuser1"=>{"cn"=>"testuser1", "groupname"=>"users", "uidNumber"=>1000, "homeDirectory"=>"/home/testuser1"},
+										"testuser2"=>{"cn"=>"testuser2", "groupname"=>"users", "uidNumber"=>1010, "homeDirectory"=>"/home/testuser1"}}
 
-    short_parameters	= {
-	"index"			=> ["s", "uid"],
-	"user_attributes"	=>  [ "as", [ "uid", "cn"]]
-    }
-    YastService.stubs(:Call).with("YaPI::USERS::UsersGet", short_parameters).returns({
+    YastService.stubs(:Call).with("YaPI::USERS::UsersGet", parameters).returns(result)
+
+    short_parameters	= {'user_attributes' => ['as', ['uid', 'cn']],
+      'type' => 'local', 'index' => ['s', 'uid']}
+    short_result = {
 	"testuser1"=>{"cn"=>"testuser1", "uid" => "testuser1"},
 	"testuser2"=>{"cn"=>"Test user2", "uid" => "testuser2"}}
-    )
+    YastService.stubs(:Call).with("YaPI::USERS::UsersGet", short_parameters).returns(short_result)
   end
 
   def test_user
