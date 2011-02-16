cd1828d8e6462f3fc8d0861b3bd6dea6abb321b2
Stefan Schubert <schubi@suse.de>
2011-02-16 11:05:12 +0100

    handle exceptions

--- webservice/lib/yast_cache.rb
+++ webservice/lib/yast_cache.rb
@@ -20,41 +20,47 @@ require 'digest/md5'
 
 class YastCache
 
-  def YastCache.reset(key)
+  def YastCache.reset(key, delay = 0)
     Rails.cache.delete(key)
-    re_load = Rails.cache.exist?(key) ?  true : false
-    Delayed::Job.enqueue(PluginJob.new(key),0, 3.seconds.from_now )
+    Delayed::Job.enqueue(PluginJob.new(key),0, (delay).seconds.from_now )
   end
     
   def YastCache.fetch(key, options = {})
-    re_load = false
-    re_load = true if Rails.cache.exist?(key)
+    job_delay = 3
+    raised_exception = nil
+    re_load = Rails.cache.exist?(key) ?  true : false
     if  block_given?
       ret = Rails.cache.fetch(key, options) {
-        block_ret = yield
-        if block_ret == nil
-          #no data found -> remove entry from the cache table
-          cache_data = DataCache.all(:conditions => "path = '#{key}'")
-          cache_data.each { |cache|
-            cache.destroy
-          } unless cache_data.blank? 
-        else
-          #update MD5 if needed
-          md5 = Digest::MD5.hexdigest(block_ret.to_json)
-          cache_data = DataCache.all(:conditions => "path = '#{key}' AND ( refreshed_md5 is NULL OR refreshed_md5 != '#{md5}')")
-          cache_data.each { |cache|
-            cache.refreshed_md5 = md5
-            cache.picked_md5 = md5 if cache.picked_md5.blank?
-            cache.save
-          } unless cache_data.blank? 
+        block_ret = nil
+        begin
+          block_ret = yield
+          if block_ret == nil
+            #no data found -> remove entry from the cache table
+            cache_data = DataCache.all(:conditions => "path = '#{key}'")
+            cache_data.each { |cache|
+              cache.destroy
+            } unless cache_data.blank? 
+          else
+            #update MD5 if needed
+            md5 = Digest::MD5.hexdigest(block_ret.to_json)
+            cache_data = DataCache.all(:conditions => "path = '#{key}' AND ( refreshed_md5 is NULL OR refreshed_md5 != '#{md5}')")
+            cache_data.each { |cache|
+              cache.refreshed_md5 = md5
+              cache.picked_md5 = md5 if cache.picked_md5.blank?
+              cache.save
+            } unless cache_data.blank? 
+          end
+        rescue Exception => raised_exception
+          Rails.logger.error "YastCache.fetch(#{key}) failed: #{raised_exception.inspect}"        
+          Rails.logger.error "Trying again in #{job_delay} seconds" if reload
         end
         block_ret
       }
     else
       ret = Rails.cache.fetch(key, options)
     end
-    YastCache.reset(key) if re_load #add reload into the job queue
-     
+    YastCache.reset(key,job_delay) if re_load #add reload into the job queue
+    raise raised_exception unless raised_exception.nil? #raising exception to the next level
     ret
   end
 end
