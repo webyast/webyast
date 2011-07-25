be1b4fd63ef668675eb0c2ccca97a3837c95bac4
vlewin <vlewin@suse.de>
2011-07-25 11:11:11 +0200

    - turned on javascript compression for webyst-base-ui

--- webyast/config/environment.rb
+++ webyast/config/environment.rb
@@ -46,7 +46,7 @@ init = Rails::Initializer.run do |config|
   #Set JSMIN constant to true/false 
   #true: load minified javascript files for speed up
   #false: load source javascript files for debugging
-  JSMIN = false
+  JSMIN = true
 
   #just for test
   #ENV['DISABLE_INITIALIZER_FROM_RAKE'] = 'false'
