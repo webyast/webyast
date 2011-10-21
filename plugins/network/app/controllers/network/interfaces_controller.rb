f48b95345650acc8f57c22b8f203f5b8070a6c80
vlewin <vlewin@suse.de>
2011-10-21 12:02:17 +0200

    - Cleanup: use local variables instead of instance variables (Justus Winter authored July 26, 2011)

--- plugins/network/app/controllers/network/interfaces_controller.rb
+++ plugins/network/app/controllers/network/interfaces_controller.rb
@@ -1,20 +1,20 @@
 #--
 # Copyright (c) 2009 Novell, Inc.
-# 
+#
 # All Rights Reserved.
-# 
+#
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of version 2 of the GNU General Public License
 # as published by the Free Software Foundation.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, contact Novell, Inc.
-# 
+#
 # To contact Novell about this file by physical or electronic mail,
 # you may find current contact information at www.novell.com
 #++
@@ -34,18 +34,18 @@ class Network::InterfacesController < ApplicationController
     if root == nil
       raise InvalidParameters.new :interface => "Missing", :interfaces => "Missing"
     end
-    @iface = Interface.new(root)
-    @iface.save!
+    iface = Interface.new(root)
+    iface.save!
     show
   end
 
   # Shows interface settings. Requires read permission for network YaPI.
   def show
-    @ifce = Interface.find(params[:id])
+    iface = Interface.find(params[:id])
 
     respond_to do |format|
-      format.xml { render :xml => @ifce.to_xml( :root => "interface", :dasherize => false ) }
-      format.json { render :json => @ifce.to_json }
+      format.xml { render :xml => iface.to_xml( :root => "interface", :dasherize => false ) }
+      format.json { render :json => iface.to_json }
     end
   end
 
@@ -54,7 +54,8 @@ class Network::InterfacesController < ApplicationController
    respond_to do |format|
      format.xml { render :xml => ifaces_a.to_xml( :root => "interfaces", :dasherize => false ) }
      format.json { render :json => ifaces_a.to_json }
-   end    
+   end
   end
 
 end
+
