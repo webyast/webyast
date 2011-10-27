94d4b7063d60b0525354c67778df1a88ed3b537c
Stefan Schubert <schubi@suse.de>
2011-10-27 14:11:36 +0200

    fixes for merge

--- plugins/example_plugin/package/exampleService.rb
+++ plugins/example_plugin/package/exampleService.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-
+require 'rubygems'
 require 'dbus'
 
 # Choose the bus (could also be DBus::session_bus, which is not suitable for a system service)
@@ -8,22 +8,23 @@ bus = DBus::system_bus
 service = bus.request_service("example.service")
 
 class ExampleService < DBus::Object
-  FILENAME = "/tmp/exampleservicefile"
+  FILENAME = "/var/log/YaST2/example_file"
   # Create an interface.
   dbus_interface "example.service.Interface" do
     # Define D-Bus methods of the service
     # This method reads whole file which name it gets as a parameter and returns its contents
     dbus_method :read, "out contents:s" do
+      out = ""
       begin
         File.open(FILENAME, "r") {|f| out = f.read }
       rescue
-        out = "NOTHING"
+        out = "<empty>"
       end
       [out] #return value must be array, as DBus allow multiple return value, so it expect array of return values
     end
     # This method dumps a string into a file
     dbus_method :write, "in contents:s" do |contents|
-      File.open(FILENAME, 'w') {|f| f.write(contents) } # XXX tom: insecure tmp file handling
+      File.open(FILENAME, 'w') {|f| f.write(contents) } 
       []
     end
   end
