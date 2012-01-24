ea2e1a92eadaa0171abe98e390a45f06eac47f2d
Ladislav Slezak <lslezak@novell.com>
2012-01-24 14:38:24 +0100

    FastGettext enhancemenets
    - move the FastGettext initialization to a separate initializer file
    - added more locale fallbacks (e.g. "pt_BR" -> "pt-BR")
    - include file names and line numbers in POT files

--- webyast/config/initializers/fast_gettext.rb
+++ webyast/config/initializers/fast_gettext.rb
@@ -1,2 +1,36 @@
+
+# put 'en_US' as first, the first item is used as a fallback
+# when requested locale (via ?locale= URL parameter) is not found
+FastGettext.default_available_locales = ["en_US"]
+
+# get available locales automatically
+Dir[File.join(File.dirname(__FILE__), '..', '..', 'locale', "/*/LC_MESSAGES/*.mo")].each do |l|
+  if l.match(/\/([^\/]+)\/LC_MESSAGES\/.*\.mo$/) && !FastGettext.default_available_locales.include?($1)
+    FastGettext.default_available_locales << $1
+  end
+end
+
+# uncomment for debugging - pretend all supported (?) translations available
+# FastGettext.default_available_locales = ["en_US","ar","cs","de","es","fr","hu","it","ja","ko","nl","pl","pt_BR","ru","sv","zh_CN","zh_TW"]
+
+Rails.logger.info "Available translations: #{FastGettext.default_available_locales.inspect}"
+
+# enable fallback handling
 I18n::Backend::Simple.include(I18n::Backend::Fallbacks)
+
+# set some locale fallbacks needed for ActiveRecord translations
+# located in rails_i18n gem (e.g. there is en-US.yml translation)
 I18n.fallbacks[:"en_US"] = [:"en-US", :en]
+I18n.fallbacks[:"en_GB"] = [:"en-GB", :en]
+I18n.fallbacks[:"pt_BR"] = [:"pt-BR", :pt]
+I18n.fallbacks[:"zh_CN"] = [:"zh-CN"]
+I18n.fallbacks[:"zh_TW"] = [:"zh-TW"]
+I18n.fallbacks[:"sv"] = [:"sv-SE"]
+
+FastGettext.add_text_domain 'webyast-base', :path => 'locale'
+FastGettext.default_text_domain = 'webyast-base'
+
+# configure default msgmerge parameters (the default contains "--no-location" option
+# which removes code lines from the final POT file)
+Rails.application.config.gettext_i18n_rails.msgmerge = ["--sort-output", "--no-wrap"]
+
