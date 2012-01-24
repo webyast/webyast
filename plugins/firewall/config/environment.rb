6892d248181299ce5c93ef5c9c6b32e135e614cf
Stefan Schubert <schubi@suse.de>
2012-01-24 15:03:27 +0100

    cleanup

--- plugins/firewall/config/environment.rb
+++ plugins/firewall/config/environment.rb
@@ -1,8 +1,4 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
-FastGettext.add_text_domain 'webyast-firewall', :path => 'locale'
-FastGettext.default_text_domain = 'webyast-firewall'
-# Initialize the rails application
 Firewall::Application.initialize!
 
