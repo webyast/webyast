bf8a39eb2f4660460675eac78d96543a18654348
Ladislav Slezak <lslezak@novell.com>
2012-01-24 15:48:03 +0100

    moved fast_gettext code from application.rb to initializers/fast_gettext.rb
    - improved available locales detection

--- webyast/config/initializers/fast_gettext.rb
+++ webyast/config/initializers/fast_gettext.rb
@@ -3,12 +3,27 @@
 # when requested locale (via ?locale= URL parameter) is not found
 FastGettext.default_available_locales = ["en_US"]
 
-# get available locales automatically
-Dir[File.join(File.dirname(__FILE__), '..', '..', 'locale', "/*/LC_MESSAGES/*.mo")].each do |l|
-  if l.match(/\/([^\/]+)\/LC_MESSAGES\/.*\.mo$/) && !FastGettext.default_available_locales.include?($1)
-    FastGettext.default_available_locales << $1
+repos = [FastGettext::TranslationRepository.build('webyast-base', :path => 'locale')]
+Rails::Engine::Railties.engines.each do |engine|
+  if engine.class.to_s.match /^WebYaST::.*Engine$/
+    mo_files = Dir.glob(File.join(engine.config.root, "**", "*.mo"))
+
+    mo_files.each do |l|
+      if l.match(/\/([^\/]+)\/LC_MESSAGES\/.*\.mo$/) && !FastGettext.default_available_locales.include?($1)
+        FastGettext.default_available_locales << $1
+      end
+    end
+
+    if mo_files.size > 0
+      locale_path = File.dirname(File.dirname(File.dirname(mo_files.first)))
+      repos << FastGettext::TranslationRepository.build(File.basename(mo_files.first, ".mo"),
+                                                        :path=>locale_path)
+    end
   end
 end
+
+FastGettext.add_text_domain 'combined', :type=>:chain, :chain=>repos
+FastGettext.default_text_domain = 'combined'
 
 # uncomment for debugging - pretend all supported (?) translations available
 # FastGettext.default_available_locales = ["en_US","ar","cs","de","es","fr","hu","it","ja","ko","nl","pl","pt_BR","ru","sv","zh_CN","zh_TW"]
@@ -26,9 +41,6 @@ I18n.fallbacks[:"pt_BR"] = [:"pt-BR", :pt]
 I18n.fallbacks[:"zh_CN"] = [:"zh-CN"]
 I18n.fallbacks[:"zh_TW"] = [:"zh-TW"]
 I18n.fallbacks[:"sv"] = [:"sv-SE"]
-
-FastGettext.add_text_domain 'webyast-base', :path => 'locale'
-FastGettext.default_text_domain = 'webyast-base'
 
 # configure default msgmerge parameters (the default contains "--no-location" option
 # which removes code lines from the final POT file)
