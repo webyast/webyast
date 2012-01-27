5c4bb2943a5a806593f82fe6611ebeae815b70c1
vlewin <vlewin@suse.de>
2012-01-27 15:28:05 +0100

    fix warning: already initialized constant set ENV to test after environment.rb

--- webyast/test/test_helper.rb
+++ webyast/test/test_helper.rb
@@ -16,10 +16,12 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #++
 
-ENV["RAILS_ENV"] = "test"
+
 require File.expand_path('../../config/environment', __FILE__)
 require File.join(File.dirname(__FILE__),"plugin_basic_tests")
 require 'rails/test_help'
+
+ENV["RAILS_ENV"] = "test"
 
 class ActionController::TestCase
   include Devise::TestHelpers
@@ -36,16 +38,16 @@ class ActiveSupport::TestCase
 
 end
 
-## use a different DB for tests -  needed during RPM build
-#if !ENV['TEST_DB_PATH'].nil? && ENV['RAILS_ENV'] == 'test'
-#  Rails.logger.debug "Using DB file for tests: #{ENV['TEST_DB_PATH']}"
+# use a different DB for tests -  needed during RPM build
+if !ENV['TEST_DB_PATH'].nil? && ENV['RAILS_ENV'] == 'test'
+  Rails.logger.debug "Using DB file for tests: #{ENV['TEST_DB_PATH']}"
 
-#  # read the current config
-#  dbconf = Rails::Configuration.new.database_configuration['test']
+  # read the current config
+  dbconf = Rails::Configuration.new.database_configuration['test']
 
-#  # update the config and make a new DB connection
-#  require 'active_record'
-#  dbconf['database'] = ENV['TEST_DB_PATH']
-#  ActiveRecord::Base.establish_connection(dbconf)
-#  ActiveRecord::Base.connection
-#end
+  # update the config and make a new DB connection
+  require 'active_record'
+  dbconf['database'] = ENV['TEST_DB_PATH']
+  ActiveRecord::Base.establish_connection(dbconf)
+  ActiveRecord::Base.connection
+end
