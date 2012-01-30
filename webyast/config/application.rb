cf9f48a3b624ed57e99c9dffefa4031f5a989f85
vlewin <vlewin@suse.de>
2012-01-30 13:17:19 +0100

    use layout/application as default layout

--- webyast/config/application.rb
+++ webyast/config/application.rb
@@ -21,12 +21,8 @@ module Webyast
     config.assets.enabled = true
     config.assets.version = '1.0'
     config.secret_token = '9d11bfc98abcf9799082d9c34ec94dc1cc926f0f1bf4bea8c440b497d96b14c1f712c8784d0303ee7dd69e382c3e5e4d38d4c56d1b619eae7acaa6516cd733b1'
-    config.to_prepare do
-      Devise::SessionsController.layout "main"
-    end
     # remove quiet_assets initializer when this works
     # issue #2639
     #config.assets.logger = nil
   end
 end
-
