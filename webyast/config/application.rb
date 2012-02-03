e77eaeaca32f7700bb1c8eec1d5d0ab514b18bac
Ladislav Slezak <lslezak@novell.com>
2012-02-03 17:27:01 +0100

    config/application.rb - disable initialization on assets precompile
    results in slighly faster assets compilation

--- webyast/config/application.rb
+++ webyast/config/application.rb
@@ -21,6 +21,11 @@ module Webyast
     config.assets.enabled = true
     config.assets.version = '1.0'
     config.secret_token = '9d11bfc98abcf9799082d9c34ec94dc1cc926f0f1bf4bea8c440b497d96b14c1f712c8784d0303ee7dd69e382c3e5e4d38d4c56d1b619eae7acaa6516cd733b1'
+
+    # for a slighly faster asset compilation
+    # (see http://guides.rubyonrails.org/asset_pipeline.html)
+    config.assets.initialize_on_precompile = false
+
     config.to_prepare do
       Devise::SessionsController.layout "application"
     end
