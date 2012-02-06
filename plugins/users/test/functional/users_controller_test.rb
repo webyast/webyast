f8eadfdcbf7ef457ec0667f8ec72d4c85f417f59
vlewin <vlewin@suse.de>
2012-02-06 15:05:15 +0100

    fix user/group unit test cases

--- plugins/users/test/functional/users_controller_test.rb
+++ plugins/users/test/functional/users_controller_test.rb
@@ -1,41 +1,41 @@
 #--
 # Copyright (c) 2009-2010 Novell, Inc.
-# 
+#
 # All Rights Reserved.
-# 
+#
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of version 2 of the GNU General Public License
 # as published by the Free Software Foundation.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, contact Novell, Inc.
-# 
+#
 # To contact Novell about this file by physical or electronic mail,
 # you may find current contact information at www.novell.com
 #++
 
 require File.expand_path(File.dirname(__FILE__) + "/../test_helper")
+require File.join(RailsParent.parent, "test","devise_helper")
 require 'test/unit'
 require 'mocha'
 
 
 class UsersControllerTest < ActionController::TestCase
-  fixtures :accounts
   def setup
+    devise_sign_in
     @controller = UsersController.new
     @request = ActionController::TestRequest.new
     # http://railsforum.com/viewtopic.php?id=1719
     @request.session[:account_id] = 1 # defined in fixtures
-
-    User.stubs(:find_all).returns([])    
+    User.stubs(:find_all).returns([])
   end
-  
-  
+
+
   test "access index" do
     mime = Mime::HTML
     @request.accept = mime.to_s
@@ -50,7 +50,7 @@ class UsersControllerTest < ActionController::TestCase
     get :index
     assert_equal mime.to_s, @response.content_type
   end
-  
+
   test "access index json" do
     mime = Mime::JSON
     @request.accept = mime.to_s
@@ -94,7 +94,7 @@ class UsersControllerTest < ActionController::TestCase
    post :update, {:user => { :id => "schubi5", :cn => "schubi5" }}
    assert_response :success
    assert flash.empty?
-  end  
+  end
 
 
 end
