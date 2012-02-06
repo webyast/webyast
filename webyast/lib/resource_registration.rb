bfbd1d3114e169c1a9153ff4623208e7b145bb09
vlewin <vlewin@suse.de>
2012-02-06 14:28:17 +0100

    add "register" action back to ResourceRegistration class (needed by services module)

--- webyast/lib/resource_registration.rb
+++ webyast/lib/resource_registration.rb
@@ -1,4 +1,4 @@
-#--
+#
 # Webyast framework
 #
 # Copyright (C) 2009, 2010 Novell, Inc.
@@ -13,7 +13,7 @@
 #
 #   You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 021101301 USA
 #++
 
 require 'singleton'
@@ -65,4 +65,91 @@ class ResourceRegistration
       end
     end
   end
+
+
+  private
+
+  def self.error msg
+    if @in_production
+      log.error msg
+      return
+    else
+      raise ResourceRegistrationFormatError.new( msg )
+    end
+  end
+
+
+
+  # register a (.yaml) resource description
+  # optionally the interface and controller can be passed
+  # otherwise they are read from the yml file
+
+  def register(file, interface = nil, controller = nil)
+    require 'yaml'
+    name = name || File.basename(file, ".*")
+
+    begin
+      resource = YAML.load(File.open(file)) || Hash.new
+    rescue Exception => e
+      raise # reraise
+    end
+
+    error "#{file} has wrong format" unless resource.is_a? Hash
+
+    # interface: can override
+    interface = resource['interface'] || interface
+
+    error "#{file} does not specify interface" unless interface
+    error "#{file}: interface is not a qualified name" unless interface =~ %r{((\w+)\.)+(\w+)}
+
+    name = interface.split(".").pop
+
+    # controller: must be given
+    controller = resource['controller'] || controller
+    error "#{file} does not specify controller" unless controller
+
+    # policy: is optional, interface is used otherwise
+    policy = resource["policy"]
+
+    # singular: is optional, defaults to false
+    singular = resource["singular"] || false
+
+    # cache_enabled: is optional, default to true
+    unless resource["cache"].blank?
+      cache_enabled = resource["cache"]["enabled"]
+    else
+      cache_enabled =  true
+    end
+
+    # cache_priority: is optional, default to 10
+    cache_priority = (resource["cache"]["priority"].to_i unless resource["cache"].blank?) || 10
+
+    # cache_reload_after: is optional, default to 0 (no reload)
+    cache_reload_after = (resource["cache"]["reload_after"].to_i unless resource["cache"].blank?) || 0
+
+    # cache_arguments: is optional, default to ""
+    cache_arguments = (resource["cache"]["arguments"] unless resource["cache"].blank? ) || ""
+
+    error "#{file}: has nonplural interface #{interface} without being flagged as singular" if !singular and name != name.pluralize
+
+    # nested: is optional, defaults to nil
+    nested = resource["nested"]
+    error "#{file}: singular resources don't support nested" if singular and nested
+
+    resources[interface] ||= Array.new
+    resources[interface] << { :controller => controller, :singular => singular, :nested => nested, :policy => policy,
+                              :cache_enabled => cache_enabled, :cache_priority => cache_priority, :cache_reload_after => cache_reload_after,
+                              :cache_arguments => cache_arguments }
+  end
+
+
+  public
+    # reset registered resources
+    # useful for testing
+    def self.reset
+      @@resources = Hash.new
+    end
+
+
+
 end # class ResourceRegistration
