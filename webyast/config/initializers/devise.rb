7dfaaa296ae068951e8a0509dee533e7a9dd75b1
Ladislav Slezak <lslezak@novell.com>
2012-07-17 10:24:58 +0200

    enabled HTTP Basic authentication

--- webyast/config/initializers/devise.rb
+++ webyast/config/initializers/devise.rb
@@ -3,4 +3,8 @@ Devise.setup do |config|
   config.use_salt_as_remember_token = false
   config.authentication_keys = [:username]
   config.timeout_in = 120.minutes
+
+  # enable HTTP Basic authentication
+  config.http_authenticatable = true
+  config.http_authentication_realm = "WebYaST"
 end
