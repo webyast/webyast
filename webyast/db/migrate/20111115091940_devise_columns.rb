f56b5367e2c2878f78a57729ef2b4dd3e2d05aba
Ladislav Slezak <lslezak@novell.com>
2012-08-01 16:08:50 +0200

    devise fixes
    - Gemfile - removed versioned devise dependency
    - Support rubygem-devise 2.x
    - 0.3.14

--- webyast/db/migrate/20111115091940_devise_columns.rb
+++ webyast/db/migrate/20111115091940_devise_columns.rb
@@ -1,10 +1,22 @@
 
+require 'devise/version'
+
 class DeviseColumns < ActiveRecord::Migration
   def up
     change_table(:accounts) do |t|
       t.rename :login, :username
       #t.rememberable
-      t.trackable
+
+      if Devise::VERSION.match /^2\./
+        ## Trackable
+        t.integer  :sign_in_count, :default => 0
+        t.datetime :current_sign_in_at
+        t.datetime :last_sign_in_at
+        t.string   :current_sign_in_ip
+        t.string   :last_sign_in_ip
+      else
+        t.trackable
+      end
       # rememberable uses remember_token, but this field is different
       t.rename :remember_token_expires_at, :remember_created_at
       # these fields are named differently in devise
