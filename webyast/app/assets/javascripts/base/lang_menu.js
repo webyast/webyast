9a10056f7825d84f4917e2fd7e6d42bdb934d12c
Ladislav Slezak <lslezak@novell.com>
2012-08-09 12:49:57 +0200

    include also padding when positioning the lang menu

--- webyast/app/assets/javascripts/base/lang_menu.js
+++ webyast/app/assets/javascripts/base/lang_menu.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
   // position the menu exactly below the label
-  $('#lang_menu').css('top', $('#lang_current').height());
+  $('#lang_menu').css('top', $('#lang_current').outerHeight());
 
   $('#lang_current').hover(function() {
     $('#lang_menu').stop(true,true).delay(200).slideDown('fast');
