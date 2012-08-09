f72a9d86bedf81c455608cb155e03e7f2e969270
Ladislav Slezak <lslezak@novell.com>
2012-08-09 11:23:02 +0200

    lang_menu.js - position the language manu exactly below the label
    CSS positioning might not be perfect (depends on font and browser)

--- webyast/app/assets/javascripts/base/lang_menu.js
+++ webyast/app/assets/javascripts/base/lang_menu.js
@@ -1,5 +1,8 @@
 
 $(document).ready(function() {
+  // position the menu exactly below the label
+  $('#lang_menu').css('top', $('#lang_current').height());
+
   $('#lang_current').hover(function() {
     $('#lang_menu').stop(true,true).delay(200).slideDown('fast');
   }, function() {
