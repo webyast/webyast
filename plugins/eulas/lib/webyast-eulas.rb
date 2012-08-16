ae4a2ff96c2fe848cf3b5a5b5bcbc752b0030076
Ladislav Slezak <lslezak@novell.com>
2012-08-16 14:03:14 +0200

    eulas - removed unused ActionDispatch::Static middleware (there is no public/ directory)
    - 0.3.3

--- plugins/eulas/lib/webyast-eulas.rb
+++ plugins/eulas/lib/webyast-eulas.rb
@@ -1,7 +1,4 @@
 module WebYaST
   class EulasEngine < Rails::Engine
-    initializer "static assets" do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
-    end
   end
 end
