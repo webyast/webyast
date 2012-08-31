b9b5d94393ffd48bad9fcaaa68dfd57290c25b07
Ladislav Slezak <lslezak@novell.com>
2012-08-31 08:58:27 +0200

    status - fixed routing

--- plugins/status/config/routes.rb
+++ plugins/status/config/routes.rb
@@ -2,9 +2,11 @@ WebYaST::StatusEngine.routes.draw do
   resources :status do
     collection do
       get :show_summary
-      get :edit, :as => :html
+      get :edit
     end
   end
   
-  resources :metrics
+  resources :metrics, :only => [:index, :show]
+  resources :logs, :only => [:index, :show]
+  resources :graphs, :only => [:index, :show, :update]
 end
