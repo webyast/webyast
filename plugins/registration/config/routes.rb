aabf454f8b36cf97a3b02378ddc59db3e4c1d9c0
Ladislav Slezak <lslezak@novell.com>
2012-09-17 13:39:33 +0200

    registration - fixed routing (fixed skipping registration) (bnc#779580)

--- plugins/registration/config/routes.rb
+++ plugins/registration/config/routes.rb
@@ -1,3 +1,12 @@
 WebYaST::RegistrationEngine.routes.draw do
-  resources :registration
+  resources :registration, :only => [:index, :show, :create, :update] do
+    collection do
+      # TODO FIXME: ??? replace this by parameters, e.g. index.html?reregister=true
+      #   POST register.html?reregister=true
+      get 'skip'
+      get 'register'
+      get 'reregister'
+      get 'reregisterupdate'
+    end
+  end
 end
