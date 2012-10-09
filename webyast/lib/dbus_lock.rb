29f4524557cd1786025fa7f709976790a2cfac8c
Ladislav Slezak <lslezak@novell.com>
2012-10-09 17:56:33 +0200

    dbus_lock.rb - added documentation and license header

--- webyast/lib/dbus_lock.rb
+++ webyast/lib/dbus_lock.rb
@@ -1,6 +1,28 @@
-
+#--
+# Webyast framework
+#
+# Copyright (C) 2012 Novell, Inc.
+#   This library is free software; you can redistribute it and/or modify
+# it only under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#++
 
 require 'singleton'
+
+# ruby-dbus bindings are NOT thread safe
+# no DBus call can be run in parallel (causes a hard crash)
+#
+# DbusLock ensures that a DBus request runs in a critical section
+# guarded by a Mutex
 
 class DbusLock
 
@@ -19,11 +41,14 @@ class DbusLock
       yield
     end
   end
-  
+
+  # just a shortcut for the instance method
   def self.locked?
     DbusLock.instance.locked?
   end
 
+  # any DBus call has to wrapped in DbusLock.synchronize call
+  # which ensures that only one process in using DBus at a time
   def self.synchronize
     Rails.logger.info "Waiting for DBus lock... (#{caller(2).first})"
 
