34cfab46d3f3f91888a01a7ed0dcdbeba4170699
Ladislav Slezak <lslezak@novell.com>
2012-10-24 13:46:15 +0200

    cleanup - removed unused "progress_bar" helper
    use jQueryUI progressbar instead - more functions, much better look
    (see http://jqueryui.com/progressbar)

--- webyast/app/helpers/view_helpers/html_helper.rb
+++ webyast/app/helpers/view_helpers/html_helper.rb
@@ -188,21 +188,6 @@ EOF2
   html.html_safe
   end
 
-  def progress_bar(percent, width = '150px', height = '1.4em')
-    # display only 0-100% range
-    percent = 100 if percent > 100
-    percent = 0 if percent < 0
-
-    html = <<-EOF_PROGRESS
-    <div class="progress_bar">
-      <div class="progress_bar_percent" style="width: #{width}; line-height: #{height}">#{percent.to_i}%</div>
-      <div class="progress_bar_frame" style="width: #{width}; height: #{height};">
-        <div class="progress_bar_progress" style="width: #{percent.to_i}%; height: #{height};"/>
-      </div>
-    </div>
-EOF_PROGRESS
-  end
-
   # Encode an input string to a string which can be safely used as
   # an HTML element id without escaping problematic symbols.
   # The result contains only [a-zA-Z0-9_]* characters and can be used in jQuery
