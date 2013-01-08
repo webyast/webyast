736192d75ded360e983e55532a24a19e7507e6ea
Ladislav Slezak <lslezak@novell.com>
2013-01-08 10:21:00 +0100

    network - added routing to REST API controllers

--- plugins/network/config/routes.rb
+++ plugins/network/config/routes.rb
@@ -1,4 +1,11 @@
 WebYaST::NetworkEngine.routes.draw do
   match "/network/iface" => "network#iface", :as => :ajax
   match "/network/partial" => "network#partial", :as => :partial
+
+  namespace :network do
+    resources :interfaces, :only => [:index, :show, :update]
+    resource :hostname, :controller => :hostname, :only => [:create, :show, :update]
+    resource :dns, :controller => :dns, :only => [:create, :show, :update]
+    resources :routes, :only => [:index, :show, :update]
+  end
 end
