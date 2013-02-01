b498ee472b55f65a30571321e50e452bb177bb09
Vladimir Moravec <vmoravec@suse.com>
2013-02-01 10:52:44 +0100

    Vim doesnt like empty spaces

--- plugins/time/app/models/systemtime.rb
+++ plugins/time/app/models/systemtime.rb
@@ -1,20 +1,20 @@
 #--
 # Copyright (c) 2009-2010 Novell, Inc.
-# 
+#
 # All Rights Reserved.
-# 
+#
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of version 2 of the GNU General Public License
 # as published by the Free Software Foundation.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, contact Novell, Inc.
-# 
+#
 # To contact Novell about this file by physical or electronic mail,
 # you may find current contact information at www.novell.com
 #++
@@ -24,8 +24,8 @@
 # Main goal is handle YaPI specific calls and data formats. Provides cleaned
 # and well defined data.
 
-# CACHING makes currently no sense cause the current time has to be 
-# evaluate everytime which will be also made by YaST:YAPI 
+# CACHING makes currently no sense cause the current time has to be
+# evaluate everytime which will be also made by YaST:YAPI
 # (It is not so easy as it sounds :-))
 
 require 'base'
@@ -73,12 +73,12 @@ class Systemtime < BaseModel::Base
     @date = timedate[0..timedate.index(" - ")-1]
     #convert date to format for datepicker
     @date.sub!(/^(\d+)-(\d+)-(\d+)/,'\3/\2/\1')
-    @utcstatus = 
+    @utcstatus =
     case response["utcstatus"]
       when "UTC" then true
       when "local" then false
       when "UTCOnly" then nil
-      else 
+      else
         Rails.logger.warn "Unknown key in utcstatus #{response["utcstatus"]}"
         nil #set nill, maybe exception???
     end
@@ -170,6 +170,4 @@ class Systemtime < BaseModel::Base
     @date = nil
     @time = nil
   end
-
-
 end
