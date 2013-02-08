b268098bb4719ab3354a66064dd00cacd45bb1e5
Vladimir Moravec <vmoravec@suse.com>
2013-02-08 14:22:59 +0100

    Improve the structure of the controller

--- plugins/time/app/controllers/time_controller.rb
+++ plugins/time/app/controllers/time_controller.rb
@@ -26,6 +26,9 @@ require 'systemtime' # RORSCAN_ITL
 # Main goal is checking permissions.
 class TimeController < ApplicationController
 
+  attr_accessor :ntp, :ntp_available, :service_available
+  private       :ntp, :ntp_available, :service_available
+
   before_filter :init
 
   rescue_from InvalidParameters, "::NtpError" do |error|
@@ -39,29 +42,10 @@ class TimeController < ApplicationController
     end
   end
 
-private
-
-  def class_exists?(class_name)
-    begin
-      cl = Module.const_get(class_name)
-      return cl.is_a?(Class)
-    rescue NameError
-      return false
-    end
-  end
-
-  def init
-    @ntp_available     = class_exists?("Ntp")
-    @service_available = class_exists?("Service")
-  end
-
-
-public
-
   def index
     authorize! :read, Time
-    if @ntp_available
-      @ntp = Ntp.find
+    if ntp_available
+      self.ntp = Ntp.find
       `pgrep -f /usr/sbin/ntpd`
       Rails.logger.info "ntpd is running: #{$?.exitstatus == 0}"
     end
@@ -81,7 +65,7 @@ public
       system_time = Systemtime.find
       case params[:systemtime][:config]
       when "manual"
-        if @service_available && @ntp_available
+        if service_available && ntp_available
           authorize! :execute, Service
           service = Service.new("ntp")
           service.save({:execute => "stop" })
@@ -89,7 +73,7 @@ public
         system_time.time = params[:systemtime][:time]
         system_time.date = params[:systemtime][:date]
       when "ntp_sync"
-        if @ntp_available
+        if ntp_available
           authorize! :synchronize, Ntp
           authorize! :setserver,   Ntp
           ntp = Ntp.find
@@ -97,7 +81,7 @@ public
           ntp.actions[:synchronize_utc] = system_time.utcstatus
           ntp.actions[:ntp_server] = params[:ntp_server]
           ntp.update
-          Service.new('ntp').save(:execute=>'start') if @service_available
+          Service.new('ntp').save(:execute=>'start') if service_available
         end
       end
     else
@@ -123,9 +107,26 @@ public
     authorize! :read, Time
     system_time = Systemtime.find  # RORSCAN_ITL
     respond_to do |format|
-      format.xml  { render :xml  => system_time.timezones.to_xml( :dasherize => false) }
-      format.json { render :json => system_time.timezones.to_json(:dasherize => false) }
+      format.xml  { render :xml  => system_time.timezones.to_xml( :root=>:timezones, :dasherize => false) }
+      format.json { render :json => system_time.timezones.to_json(:root=>:timezones, :dasherize => false) }
     end
   end
+
+  private
+
+  def class_exists?(class_name)
+    begin
+      cl = Module.const_get(class_name)
+      return cl.is_a?(Class)
+    rescue NameError
+      return false
+    end
+  end
+
+  def init
+    self.ntp_available     = class_exists?("Ntp")
+    self.service_available = class_exists?("Service")
+  end
+
 
 end
