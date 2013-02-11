9ade54addfc54afcd3beb3af33f163d45559eae5
Vladimir Moravec <vmoravec@suse.com>
2013-02-11 14:03:34 +0100

    Require mocha in test helper

--- plugins/time/test/test_helper.rb
+++ plugins/time/test/test_helper.rb
@@ -2,4 +2,5 @@
 require File.join(File.dirname(__FILE__), '..', 'config', 'rails_parent')
 require File.join(RailsParent.parent, "test","test_helper")
 require File.join(RailsParent.parent, "test","validation_assert")
+require 'mocha'
 
