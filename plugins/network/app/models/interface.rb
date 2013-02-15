60debad5c03864afeeb73a60ffba9806e470a6f1
Josef Reidinger <jreidinger@suse.cz>
2013-02-15 12:44:41 +0000

    fix passing network prefix instead of netmask

--- plugins/network/app/models/interface.rb
+++ plugins/network/app/models/interface.rb
@@ -56,7 +56,13 @@ class Interface < BaseModel::Base
   public
 
   def netmask_to_cidr(netmask)
-    return IPAddr.new(netmask).to_i.to_s(2).count("1")
+    if netmask =~ /\A\d{1,2}\Z/
+      return netmask #we get already cidr
+    elsif netmask =~ /\A\/\d{1,2}\Z/
+      return netmask[1..-1] #we already get it with starting slash
+    else
+      return IPAddr.new(netmask).to_i.to_s(2).count("1")
+    end
   end
 
   def self.cidr_to_netmask(cidr)
