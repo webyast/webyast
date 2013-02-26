3797c18ec3536bebce4057871da6cbe097842d17
Vladimir Moravec <vmoravec@suse.com>
2013-02-26 15:13:48 +0100

    Fix Role namespacing

--- plugins/users/app/controllers/users_controller.rb
+++ plugins/users/app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
 
   def save_roles (userid,roles_string)
     user_roles = roles_string.split(",")
-    Role.find(:all).each do |role|
+    ::Role.find(:all).each do |role|
       if role.users.include?(userid) && !user_roles.include?(role.name)
         role.users.delete(userid)
         role.save
